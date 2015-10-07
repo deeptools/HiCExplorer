584b463dc84c63aed3f59085afbc95dd5efa2b4d
Fidel Ramirez <ramirez@ie-freiburg.mpg.de>
2015-10-07 14:55:39 +0200

    make find rest site not case sensitive.

--- hicexplorer/findRestSite.py
+++ hicexplorer/findRestSite.py
@@ -9,7 +9,9 @@ from Bio.Alphabet import generic_dna
 def parse_arguments(args=None):
     parser = argparse.ArgumentParser(description='returns a bed file'
                                      'containing the positions '
-                                     'of a string, giving a fasta file')
+                                     'of a string, giving a fasta file',
+                                     usage='An example usage is: %(prog)s --fasta mm10.fa '
+                                           '--searchPattern AAGCTT -o rest_site_positions.bed')
     
     # define the arguments
     parser.add_argument('--fasta', '-f',
@@ -19,7 +21,8 @@ def parse_arguments(args=None):
 
     # define the arguments
     parser.add_argument('--searchPattern', '-p',
-                        help='Search pattern',
+                        help='Search pattern. For example for HindII this pattern is "AAGCTT". '
+                             'Both, forward and reverse strand are searched for a match.',
                         required=True)
 
     parser.add_argument('--outFile', '-o',
@@ -36,11 +39,11 @@ def main():
     rev_compl = str(Seq(args.searchPattern, generic_dna).complement())
     for record in SeqIO.parse(args.fasta, 'fasta', generic_dna):
         # find all the occurrences of pattern
-        for match in re.finditer(args.searchPattern, str(record.seq)):
-            args.outFile.write('{}\t{}\t{}\n'.format(record.name, 
+        for match in re.finditer(args.searchPattern, str(record.seq), re.IGNORECASE):
+            args.outFile.write('{}\t{}\t{}\t.\t0\t+\n'.format(record.name,
                                                      match.start(),
                                                      match.end()))
-        for match in re.finditer(rev_compl, str(record.seq)):
-            args.outFile.write('{}\t{}\t{}\n'.format(record.name, 
+        for match in re.finditer(rev_compl, str(record.seq), re.IGNORECASE):
+            args.outFile.write('{}\t{}\t{}\t.\t0\t.\n'.format(record.name,
                                                      match.start(),
                                                      match.end()))
