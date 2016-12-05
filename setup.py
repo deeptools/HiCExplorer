1bce36c1bb82cb954828c78f887ccaad4bbcc30f
Fidel Ramirez <fidel.ramirez@gmail.com>
2016-12-05 17:41:29 +0100

    increased the version number required for matplotlib after #47

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ setup(
     install_requires=[
         "numpy >= 1.10.4",
         "scipy >= 0.17.1",
-        "matplotlib >= 1.3.1",
+        "matplotlib >= 1.5.3",
         "pysam >= 0.8.3",
         "intervaltree >= 2.1.0",
         "biopython >= 1.65",
