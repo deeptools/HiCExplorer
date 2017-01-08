c26b55cbc720df7c708889e01513432fe49f0e87
Björn Grüning <bjoern@gruenings.eu>
2017-01-08 22:46:40 +0100

    Create .planemo.sh

---
+++ .planemo.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+blah=`mktemp -d`
+~/foo/bin/planemo database_create galaxy
+~/foo/bin/planemo conda_init --conda_prefix $blah/conda
+export PATH=$blah/conda/bin:$PATH
+conda create -y -c bioconda --name deeptools_galaxy numpy scipy matplotlib=1.5.3 nose flake8 pytables biopython pysam pyBigWig intervaltree
+source activate deeptools_galaxy
+conda install -c bioconda samtools
+git clone --depth 1 https://github.com/galaxyproject/galaxy.git clone
+cd clone
+#Add the custom data types
+#sed -i '4i\    <datatype extension="deeptools_compute_matrix_archive" type="galaxy.datatypes.binary:CompressedArchive" subclass="True" display_in_upload="True"/>' config/datatypes_conf.xml.sample
+#sed -i '5i\    <datatype extension="deeptools_coverage_matrix" type="galaxy.datatypes.binary:CompressedArchive" subclass="True" display_in_upload="True"/>' config/datatypes_conf.xml.sample
+./scripts/common_startup.sh --skip-venv --dev-wheels
+cd ..
+conda uninstall -y sqlite
+pip install . 
+~/foo/bin/planemo test --galaxy_root clone --skip_venv --postgres galaxy/wrapper/
