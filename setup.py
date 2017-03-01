edaa050cc896aed1ada1aac579197e3da37d0141
vivekbhr <bhardwaj@ie-freiburg.mpg.de>
2017-03-01 11:56:44 +0100

    desperately trying stuff similar to deeptools

--- setup.py
+++ setup.py
@@ -51,6 +51,43 @@ def get_version():
             return ver
     return None
 
+class sdist(_sdist):
+
+    def run(self):
+        update_version_py()
+        self.distribution.metadata.version = get_version()
+        return _sdist.run(self)
+
+
+class install(_install):
+
+    def run(self):
+        update_version_py()
+        self.distribution.metadata.version = get_version()
+        _install.run(self)
+        return
+
+    def checkProgramIsInstalled(self, program, args, where_to_download,
+                                affected_tools):
+        try:
+            subprocess.Popen([program, args],
+                             stderr=subprocess.PIPE,
+                             stdout=subprocess.PIPE)
+            return True
+        except EnvironmentError:
+            # handle file not found error.
+            # the config file is installed in:
+            msg = "\n**{0} not found. This " \
+                  "program is needed for the following "\
+                  "tools to work properly:\n"\
+                  " {1}\n"\
+                  "{0} can be downloaded from here:\n " \
+                  " {2}\n".format(program, affected_tools,
+                                  where_to_download)
+            sys.stderr.write(msg)
+
+        except Exception as e:
+            sys.stderr.write("Error: {}".format(e))
 
 setup(
     name='HiCExplorer',
@@ -74,9 +111,9 @@ setup(
         "matplotlib >= 1.5.3",
         "pysam >= 0.8.3",
         "intervaltree >= 2.1.0",
-        "biopython >= 1.65"],
-    extras_require={
-    'bigwig' : ["tables >= 3.2.2","pyBigWig >=0.2.8"]
-    }
-
+        "biopython >= 1.65",
+        "tables >= 3.2.2",
+        "pyBigWig >=0.2.8"],
+    zip_safe=False,
+    cmdclass={'sdist': sdist, 'install': install}
 )
