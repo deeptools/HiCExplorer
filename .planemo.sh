5f950f723ccff9dc1abb1cdc43e30efe4f3370ef
Björn Grüning <bjoern@gruenings.eu>
2017-03-15 09:41:58 +0100

    cleanups

--- .planemo.sh
+++ .planemo.sh
@@ -6,13 +6,8 @@ blah=`mktemp -d`
 export PATH=$blah/conda/bin:$PATH
 conda create -y -c bioconda --name deeptools_galaxy samtools python=2.7.13 numpy scipy matplotlib=1.5.3 nose flake8 pytables biopython pysam pybigwig intervaltree
 source activate deeptools_galaxy
-#git clone --depth 1 https://github.com/galaxyproject/galaxy.git clone
-#cd clone
-#Add the custom data types
-#sed -i '4i\    <datatype extension="deeptools_compute_matrix_archive" type="galaxy.datatypes.binary:CompressedArchive" subclass="True" display_in_upload="True"/>' config/datatypes_conf.xml.sample
-#sed -i '5i\    <datatype extension="deeptools_coverage_matrix" type="galaxy.datatypes.binary:CompressedArchive" subclass="True" display_in_upload="True"/>' config/datatypes_conf.xml.sample
-#./scripts/common_startup.sh --skip-venv --dev-wheels
-#cd ..
-#conda uninstall -y sqlite
+
 pip install . 
+
+# Galaxy wrapper testing
 /home/travis/build/maxplanck-ie/HiCExplorer/foo/bin/planemo test --skip_venv --install_galaxy --no_conda_auto_install --no_conda_auto_init --galaxy_branch release_17.01 --postgres galaxy/wrapper/
