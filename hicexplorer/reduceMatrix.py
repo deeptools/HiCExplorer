16c289f832ebbc8310a1d2d0078a04f81b34cc0b
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2017-03-17 17:11:21 +0100

    pep8

--- hicexplorer/reduceMatrix.py
+++ hicexplorer/reduceMatrix.py
@@ -156,7 +156,8 @@ def reduce_matrix(matrix, bins_to_merge, use_triu=True, diagonal=False):
     # present
     num_nan = len(np.flatnonzero(np.isnan(np.array(ma.data))))
     if num_nan > 0:
-        logging.warn("*Warning*\nmatrix contains {} NaN values.".format(num_nan))
+        logging.warn(
+            "*Warning*\nmatrix contains {} NaN values.".format(num_nan))
 
     # each original col and row index is converted
     # to a new index based on the bins_to_merge.
@@ -164,9 +165,10 @@ def reduce_matrix(matrix, bins_to_merge, use_triu=True, diagonal=False):
     # then all rows whose value is 1 or 10 are given
     # as new value the index in the bins_to_merge list.
 
-    map_ = np.zeros(ma.shape[0], dtype=int) - 1     # -1 such that all cases not replaced by the next loop
-                                                    # can be identified later. Those cases that remain as -1
-                                                    # are for the rows/cols not appearing in the bins_to_merge
+    # -1 such that all cases not replaced by the next loop
+    map_ = np.zeros(ma.shape[0], dtype=int) - 1
+    # can be identified later. Those cases that remain as -1
+    # are for the rows/cols not appearing in the bins_to_merge
     for k, v in enumerate(bins_to_merge):
         for x in v:
             map_[x] = k
@@ -228,4 +230,3 @@ def reduce_matrix(matrix, bins_to_merge, use_triu=True, diagonal=False):
     matrix.eliminate_zeros()
 
     return matrix
-
