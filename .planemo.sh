94506774fa1363251b1aa1edf43b93466c3acdb1
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2017-06-01 17:55:54 +0200

    replaced virtualenv with condas environments to have planemo in python 2.7

--- .planemo.sh
+++ .planemo.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 tmp_dir=`mktemp -d`
-
-planemo_test_env/bin/planemo database_create galaxy
-planemo_test_env/bin/planemo conda_init --conda_prefix $tmp_dir/conda
+source activate python2.7
+planemo_bin='which planemo'
+source deactivate
+$planemo_bin database_create galaxy
+$planemo_bin conda_init --conda_prefix $tmp_dir/conda
 export PATH=$tmp_dir/conda/bin:$PATH
 conda create -y -c bioconda --name hicexplorer_galaxy samtools python=2.7.13 numpy scipy matplotlib=1.5.3 nose flake8 pytables biopython pysam pybigwig intervaltree future six pandas
 
@@ -13,5 +15,5 @@ pip install .
 
 
 # Galaxy wrapper testing
-planemo_test_env/bin/planemo test --skip_venv --install_galaxy --no_conda_auto_install --no_conda_auto_init --galaxy_branch release_17.01 --postgres galaxy/wrapper/
+$planemo_bin test --skip_venv --install_galaxy --no_conda_auto_install --no_conda_auto_init --galaxy_branch release_17.01 --postgres galaxy/wrapper/
 # /home/travis/build/maxplanck-ie/HiCExplorer/foo/bin/planemo test --skip_venv --install_galaxy --no_conda_auto_install --no_conda_auto_init --galaxy_branch release_17.01 --postgres galaxy/wrapper/
