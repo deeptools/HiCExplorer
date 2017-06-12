37c893bad15301d4093ed758c723c1591780f0c4
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2017-06-12 16:47:13 +0200

    Debug

--- .planemo.sh
+++ .planemo.sh
@@ -3,19 +3,20 @@
 tmp_dir=`mktemp -d`
 conda create -c bioconda -c conda-forge --name python2.7 python=2.7.13 -y
 source activate python2.7
-conda install -c bioconda -c conda-forge planemo -y
+# conda install -c bioconda -c conda-forge planemo -y
+pip install planemo
 # planemo_bin='which planemo'
 # echo $planemo_bin
 # source deactivate
 # echo $CONDA_PREFIX
 planemo database_create galaxy
-curl https://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -o miniconda.sh
-chmod +x miniconda.sh
-./miniconda.sh -b -p $tmp_dir/conda
+# curl https://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -o miniconda.sh
+# chmod +x miniconda.sh
+# ./miniconda.sh -b -p $tmp_dir/conda
 # echo $CONDA_PREFIX
-# planemo conda_init --conda_prefix $tmp_dir/conda
+planemo conda_init --conda_prefix $tmp_dir/conda
 # export PATH=$tmp_dir/conda/bin:$PATH
-conda install -y -c bioconda samtools python=2.7.13 numpy scipy matplotlib=2.0.0 nose flake8 pytables biopython pysam pybigwig intervaltree future six pandas
+# conda install -y -c bioconda samtools python=2.7.13 numpy scipy matplotlib=2.0.0 nose flake8 pytables biopython pysam pybigwig intervaltree future six pandas
 
 # source activate hicexplorer_galaxy
 # echo $CONDA_PREFIX
