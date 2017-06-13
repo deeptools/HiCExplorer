bcaf247192b493befa19ad2616388059cf388aea
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2017-06-13 20:39:08 +0200

    Debug

--- .planemo.sh
+++ .planemo.sh
@@ -7,7 +7,7 @@ pip install planemo
 
 planemo database_create galaxy
 planemo conda_init --conda_prefix $tmp_dir/conda
-# planemo conda_install galaxy/wrapper --conda_prefix $tmp_dir/conda
+planemo conda_install galaxy/wrapper --conda_prefix $tmp_dir/conda
 python setup.py install
 planemo test --install_galaxy --galaxy_branch release_17.01 --skip_venv --conda_prefix $tmp_dir/conda --conda_dependency_resolution --postgres galaxy/wrapper
 source deactivate
