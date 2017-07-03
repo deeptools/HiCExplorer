60f261b21700924211fe28221b7f3b9ebf043e6a
fidelram <fidel.ramirez@gmail.com>
2017-07-03 17:49:54 +0200

    pytables (and HDF5) do not support unicode strings. Thus, pytables in python3 encodes numpy unicode strings using the byte type.  When comparing a string with an string that was read from pytables usually there is a mismatch in the `test_save_load` because the chrom names "a" != b"a". The solution for the test is to use binary types since the beginning.

--- hicexplorer/test/test_hicmatrix.py
+++ hicexplorer/test/test_hicmatrix.py
@@ -14,8 +14,8 @@ ROOT = os.path.dirname(os.path.abspath(__file__)) + "/test_data/"
 
 def test_save_load():
     outfile = '/tmp/matrix.h5'
-    cut_intervals = [('a', 0, 10, 1), ('a', 10, 20, 1),
-                     ('a', 20, 30, 1), ('a', 30, 40, 1), ('b', 40, 50, 1)]
+    cut_intervals = [(b'a', 0, 10, 1), (b'a', 10, 20, 1),
+                     (b'a', 20, 30, 1), (b'a', 30, 40, 1), (b'b', 40, 50, 1)]
     hic = hm.hiCMatrix()
     hic.nan_bins = []
     matrix = np.array([[1, 8, 5, 3, 0],
@@ -34,13 +34,13 @@ def test_save_load():
     hic.save(outfile)
 
     h5 = hm.hiCMatrix(outfile)
-
     nt.assert_equal(hic.correction_factors, h5.correction_factors)
     nt.assert_equal(hic.matrix.data, h5.matrix.data)
     nt.assert_equal(hic.matrix.indices, h5.matrix.indices)
     nt.assert_equal(hic.matrix.indptr, h5.matrix.indptr)
     nt.assert_equal(hic.nan_bins, h5.nan_bins)
-    assert hic.cut_intervals == h5.cut_intervals
+
+    nt.assert_equal(hic.cut_intervals, h5.cut_intervals)
     unlink(outfile)
 
 
