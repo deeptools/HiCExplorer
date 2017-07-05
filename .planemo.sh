773491da9bffd4a265e4c1de858552d67709822f
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2017-07-05 20:24:49 +0200

    Removed the creation of a env because it is only tested on a python 2.7 context

--- .planemo.sh
+++ .planemo.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 tmp_dir=`mktemp -d`
-conda create -c bioconda -c conda-forge --name python2.7 python=2.7.13 -y
-source activate python2.7
+# conda create -c bioconda -c conda-forge --name python2.7 python=2.7.13 -y 
+# source activate python2.7
 pip install planemo
 
 planemo database_create galaxy
@@ -10,4 +10,4 @@ planemo conda_init --conda_prefix $tmp_dir/conda
 planemo conda_install galaxy/wrapper --conda_prefix $tmp_dir/conda
 python setup.py install
 planemo test --install_galaxy --galaxy_branch release_17.01 --skip_venv --conda_prefix $tmp_dir/conda --conda_dependency_resolution --postgres galaxy/wrapper
-source deactivate
+# source deactivate
