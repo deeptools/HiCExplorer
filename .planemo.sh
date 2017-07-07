38554ccd388835404c4b3cac6950558dce059706
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2017-07-07 18:29:18 +0200

    Workaround for missing packages for planemo conda_build

--- .planemo.sh
+++ .planemo.sh
@@ -4,9 +4,9 @@ tmp_dir=`mktemp -d`
 # conda create -c bioconda -c conda-forge --name python2.7 python=2.7.13 -y 
 # source activate python2.7
 pip install planemo
-
 planemo database_create galaxy
 planemo conda_init --conda_prefix $tmp_dir/conda
+pip install --requirement requirements.txt --target $tmp_dir/conda/lib/python2.7/site-packages/
 planemo conda_build  --conda_prefix $tmp_dir/conda conda_hicexplorer_test
 planemo conda_install --conda_prefix $tmp_dir/conda --conda_use_local hicexplorer
 planemo conda_install galaxy/wrapper --conda_prefix $tmp_dir/conda --conda_use_local
