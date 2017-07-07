3c7f22f8dccf9a1907452c25938b3171c7847ee2
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2017-07-07 18:57:11 +0200

    Removed '--conda_dependency_resolution'

--- .planemo.sh
+++ .planemo.sh
@@ -5,10 +5,9 @@ tmp_dir=`mktemp -d`
 # source activate python2.7
 pip install planemo
 planemo database_create galaxy
-planemo conda_init --conda_prefix $tmp_dir/conda
-pip install --requirement requirements.txt --target $tmp_dir/conda/lib/python2.7/site-packages/
-planemo conda_build  --conda_prefix $tmp_dir/conda conda_hicexplorer_test
-planemo conda_install --conda_prefix $tmp_dir/conda --conda_use_local hicexplorer
-planemo conda_install galaxy/wrapper --conda_prefix $tmp_dir/conda --conda_use_local
-planemo test --install_galaxy --galaxy_branch release_17.01 --skip_venv --conda_prefix $tmp_dir/conda --conda_dependency_resolution --conda_use_local --postgres galaxy/wrapper
+# planemo conda_init --conda_prefix $tmp_dir/conda
+
+# planemo conda_install --conda_prefix $tmp_dir/conda --conda_use_local hicexplorer
+# planemo conda_install galaxy/wrapper --conda_prefix $tmp_dir/conda --conda_use_local
+planemo test --install_galaxy --galaxy_branch release_17.01 --skip_venv --postgres galaxy/wrapper
 # source deactivate
