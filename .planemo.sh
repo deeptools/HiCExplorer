39d3981818c53d6bd49cafe1fc9f1a25b354ae99
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2017-07-07 14:45:59 +0200

    Added planemo conda_install

--- .planemo.sh
+++ .planemo.sh
@@ -8,6 +8,7 @@ pip install planemo
 planemo database_create galaxy
 planemo conda_init --conda_prefix $tmp_dir/conda
 planemo conda_build  --conda_prefix $tmp_dir/conda conda_hicexplorer_test
+planemo clonda_install --conda_prefix $tmp_dir/conda --conda_use_local hicexplorer
 planemo conda_install galaxy/wrapper --conda_prefix $tmp_dir/conda --conda_use_local
 planemo test --install_galaxy --galaxy_branch release_17.01 --skip_venv --conda_prefix $tmp_dir/conda --conda_dependency_resolution --conda_use_local --postgres galaxy/wrapper
 # source deactivate
