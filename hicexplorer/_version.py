f42dc509e15361e4887a1da7d8874243ae10acb7
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2017-07-10 18:58:34 +0200

    Increased version number to 1.7.3

--- hicexplorer/_version.py
+++ hicexplorer/_version.py
@@ -2,4 +2,4 @@
 # This file is originally generated from Git information by running 'setup.py
 # version'. Distribution tarballs contain a pre-generated copy of this file.
 
-__version__ = '1.7.2-12-g6d2eb88'
+__version__ = '1.7.3'
