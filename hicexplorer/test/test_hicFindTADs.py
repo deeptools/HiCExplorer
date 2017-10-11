c3785e54f2c837ac08d6073bffc70c291c398651
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2017-10-11 13:48:16 +0200

    Added test case for dekker matrix, somehow bug fix #123 is not necessary anymore. Indeed it fails because of this.

--- hicexplorer/test/test_hicFindTADs.py
+++ hicexplorer/test/test_hicFindTADs.py
@@ -48,6 +48,31 @@ def test_find_TADs_fdr():
     shutil.rmtree(tad_folder)
 
 
+def test_find_TADs_fdr_dekker():
+    # full test case with build of the matrix and search for tads
+    matrix = ROOT + "dekker.txt.gz"
+    tad_folder = mkdtemp(prefix="test_case_find_tads_fdr_dekker")
+    args = "--matrix {} --minDepth 150000 --maxDepth 300000 --numberOfProcessors 2 --step 50000 \
+    --outPrefix {}/test_multiFDR_dekker \
+    --correctForMultipleTesting fdr".format(matrix, tad_folder).split()
+
+    hicFindTADs.main(args)
+
+    new = hm.hiCMatrix(tad_folder + "/test_multiFDR_dekker_zscore_matrix.h5")
+    test = hm.hiCMatrix(ROOT + 'find_TADs/dekker/multiFDR_dekker_zscore_matrix.h5')
+    nt.assert_equal(test.matrix.data, new.matrix.data)
+    nt.assert_equal(test.cut_intervals, new.cut_intervals)
+
+    print(tad_folder + "/test_multiFDR_boundaries.bed")
+    assert are_files_equal(ROOT + "find_TADs/dekker/multiFDR_dekker_boundaries.bed", tad_folder + "/test_multiFDR_dekker_boundaries.bed")
+    assert are_files_equal(ROOT + "find_TADs/dekker/multiFDR_dekker_domains.bed", tad_folder + "/test_multiFDR_dekker_domains.bed")
+    assert are_files_equal(ROOT + "find_TADs/dekker/multiFDR_dekker_tad_score.bm", tad_folder + "/test_multiFDR_dekker_tad_score.bm")
+    assert are_files_equal(ROOT + "find_TADs/dekker/multiFDR_dekker_boundaries.gff", tad_folder + "/test_multiFDR_dekker_boundaries.gff")
+    assert are_files_equal(ROOT + "find_TADs/dekker/multiFDR_dekker_score.bedgraph", tad_folder + "/test_multiFDR_dekker_score.bedgraph")
+
+    shutil.rmtree(tad_folder)
+
+
 def test_find_TADs_bonferroni():
     # reduced test case, the z-score matrix is given to decrease run time
     matrix = ROOT + "small_test_matrix.h5"
@@ -59,11 +84,6 @@ def test_find_TADs_bonferroni():
     --correctForMultipleTesting bonferroni --thresholdComparisons 0.1".format(matrix, tad_folder).split()
 
     hicFindTADs.main(args)
-
-    # new = hm.hiCMatrix(tad_folder + "/test_multiBonferroni_zscore_matrix.h5")
-    # test = hm.hiCMatrix(ROOT + 'find_TADs/multiBonferroni_zscore_matrix.h5')
-    # nt.assert_equal(test.matrix.data, new.matrix.data)
-    # nt.assert_equal(test.cut_intervals, new.cut_intervals)
 
     print(tad_folder + "/test_multiBonferroni_boundaries.bed")
     assert are_files_equal(ROOT + "find_TADs/multiBonferroni_boundaries.bed", tad_folder + "/test_multiBonferroni_boundaries.bed", pDifference=10)
@@ -87,15 +107,10 @@ def test_find_TADs_none():
 
     hicFindTADs.main(args)
 
-    # new = hm.hiCMatrix(tad_folder + "/test_multiNone_zscore_matrix.h5")
-    # test = hm.hiCMatrix(ROOT + 'find_TADs/multiNone_zscore_matrix.h5')
-    # nt.assert_equal(test.matrix.data, new.matrix.data)
-    # nt.assert_equal(test.cut_intervals, new.cut_intervals)
-
     assert are_files_equal(ROOT + "find_TADs/multiNone_boundaries.bed", tad_folder + "/test_multiNone_boundaries.bed")
     assert are_files_equal(ROOT + "find_TADs/multiNone_domains.bed", tad_folder + "/test_multiNone_domains.bed")
     # assert are_files_equal(ROOT + "find_TADs/multiNone_tad_score.bm", tad_folder + "/test_multiNone_tad_score.bm")
     assert are_files_equal(ROOT + "find_TADs/multiNone_boundaries.gff", tad_folder + "/test_multiNone_boundaries.gff")
     assert are_files_equal(ROOT + "find_TADs/multiNone_score.bedgraph", tad_folder + "/test_multiNone_score.bedgraph")
 
-    # shutil.rmtree(tad_folder)
+    shutil.rmtree(tad_folder)
