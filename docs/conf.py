ee8f49d3442b28acfab4601cf365033049a1862a
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2017-12-26 12:00:37 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ MOCK_MODULES = ['numpy', 'numpy.distutils.core', 'pandas', 'pysam', 'intervaltre
                 'scipy', 'scipy.sparse', 'scipy.stats',
                 'matplotlib', 'matplotlib.pyplot', 'matplotlib.gridspec', 'matplotlib.ticker',
                 'matplotlib.textpath', 'matplotlib.patches', 'matplotlib.colors', 'matplotlib.cm',
-                'mpl_toolkits', 'mpl_toolkits.axisartist', 'mpl_toolkits.mplot3d',
+                'mpl_toolkits', 'mpl_toolkits.axisartist', 'mpl_toolkits.mplot3d', 'mpl_toolkits.axes_grid1',
                 'Bio', 'Bio.Seq', 'Bio.Alphabet',
                 'pyBigWig', 'tables', 'pytables', 'future', 'past', 'builtins', 'past.builtins',
                 'future.utils', 'cooler', '__future__', 'logging']
