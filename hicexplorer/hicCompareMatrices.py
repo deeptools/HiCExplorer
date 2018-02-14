44e9f03b5091bf6ca751da0ce41bc1db184e2968
Gautier RICHARD <26085589+gtrichard@users.noreply.github.com>
2018-02-14 15:26:54 +0100

    hicCompareMatrices.py Opt and Required arguments

--- hicexplorer/hicCompareMatrices.py
+++ hicexplorer/hicCompareMatrices.py
@@ -12,29 +12,35 @@ log = logging.getLogger(__name__)
 def parse_arguments(args=None):
 
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-                                     description=('Takes two matrices as input, normalizes them and applies'
+                                     add_help=False,
+                                     description=('Takes two matrices as input, normalizes them and applies '
                                                   'the given operation. To normalize the matrices '
                                                   'each element is divided by the sum of the matrix.'))
 
-    parser.add_argument('--matrices', '-m',
-                        help='Name of the matrices in .h5 format to use, separated by a space.',
-                        metavar='matrix.h5',
-                        nargs=2,
-                        required=True)
+    parserRequired = parser.add_argument_group('Required arguments')
 
-    parser.add_argument('--outFileName', '-o',
-                        help='File name to save the resulting matrix. The output is '
-                             'also a .h5 file.',
-                        required=True)
+    parserRequired.add_argument('--matrices', '-m',
+                                help='Name of the matrices in .h5 format to use, separated by a space.',
+                                metavar='matrix.h5',
+                                nargs=2,
+                                required=True)
 
-    parser.add_argument('--operation',
-                        help='Operation to apply to the matrices. Options are diff, ratio, log2ratio '
-                        'default is log2ratio.',
-                        choices=['diff', 'ratio', 'log2ratio'],
-                        default='log2ratio')
+    parserRequired.add_argument('--outFileName', '-o',
+                                help='File name to save the resulting matrix. The output is '
+                                'also a .h5 file.',
+                                required=True)
 
-    parser.add_argument('--version', action='version',
-                        version='%(prog)s {}'.format(__version__))
+    parserOpt = parser.add_argument_group('Optional arguments')
+
+    parserOpt.add_argument('--operation',
+                           help='Operation to apply to the matrices.',
+                           choices=['diff', 'ratio', 'log2ratio'],
+                           default='log2ratio')
+
+    parserOpt.add_argument("-h", "--help", action="help", help="show this help message and exit")
+
+    parserOpt.add_argument('--version', action='version',
+                           version='%(prog)s {}'.format(__version__))
 
     return parser
 
