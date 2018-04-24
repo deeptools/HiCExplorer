07dab1aafd71a360d25e35024094a4a2cd929fac
Gautier RICHARD <26085589+gtrichard@users.noreply.github.com>
2018-04-24 16:26:56 +0200

    hicPlotDistVsCounts.py more pep8

--- hicexplorer/hicPlotDistVsCounts.py
+++ hicexplorer/hicPlotDistVsCounts.py
@@ -387,9 +387,9 @@ def main(args=None):
             if args.outFileData is not None:
                 x_vals = np.stack(x).T
                 y_vals = np.stack(y).T
-                table_to_export = pd.DataFrame({'Matrix': matrix_file, 
-                                                'Chromosome': chrom, 
-                                                'Distance': x_vals, 
+                table_to_export = pd.DataFrame({'Matrix': matrix_file,
+                                                'Chromosome': chrom,
+                                                'Distance': x_vals,
                                                 'Contacts': y_vals})
                 table_to_export.to_csv(args.outFileData, sep='\t')
 
