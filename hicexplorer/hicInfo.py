c32fbdf844ec063063bd052682418ca0847da197
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2018-08-23 16:08:48 +0200

    Adding features to improve the handling of cooler issues to next version. Related to cooler_correction_patch branch and issues #254 and #255

--- hicexplorer/hicInfo.py
+++ hicexplorer/hicInfo.py
@@ -3,7 +3,7 @@ import argparse
 from hicexplorer import HiCMatrix as hm
 from hicexplorer._version import __version__
 from hicexplorer.utilities import toString
-
+from hicexplorer.HiCMatrix import check_cooler
 import logging
 log = logging.getLogger(__name__)
 
@@ -24,11 +24,8 @@ $ hicInfo -m matrix1.h5 matrix2.h5 matrix3.h5
 
     parserRequired.add_argument('--matrices', '-m',
                                 help='The matrix (or multiple matrices) to get information about. '
-                                'HiCExplorer supports the following file formats: h5 (native HiCExplorer format), '
-                                'npz (format used by earlier versions of HiCExplorer), '
-                                'dekker (matrix format used in Job Dekker publications), '
-                                'and lieberman (format used by Erez Lieberman Aiden). This last formats may change '
-                                'in the future.',
+                                'HiCExplorer supports the following file formats: h5 (native HiCExplorer format) '
+                                'and cool.',
                                 nargs='+',
                                 required=True)
 
@@ -46,7 +43,7 @@ def main():
 
     args = parse_arguments().parse_args()
     for matrix in args.matrices:
-
+        # if
         hic_ma = hm.hiCMatrix(matrix)
         size = hic_ma.matrix.shape[0]
         num_non_zero = hic_ma.matrix.nnz
@@ -55,9 +52,8 @@ def main():
         num_nan_bins = len(hic_ma.nan_bins)
         min_non_zero = hic_ma.matrix.data.min()
         max_non_zero = hic_ma.matrix.data.max()
-        if not matrix.endswith("lieberman"):
-            log.debug("lieberman matrix")
-            chromosomes = list(hic_ma.chrBinBoundaries)
+
+        chromosomes = list(hic_ma.chrBinBoundaries)
 
         print("File:\t{}".format(matrix))
         print("Size:\t{:,}".format(size))
@@ -68,3 +64,5 @@ def main():
         print("Minimum (non zero):\t{}".format(min_non_zero))
         print("Maximum:\t{}".format(max_non_zero))
         print("NaN bins:\t{}".format(num_nan_bins))
+        if check_cooler(matrix):
+            hic_ma.getInformationCoolerBinNames()
