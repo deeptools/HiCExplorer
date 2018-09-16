Fixing background computation. Range is filtered out in the last step and does not influence the relative background model anymore

hicexplorer/lib/viewpoint.py
@@ -126,22 +126,41 @@ class Viewpoint():
         view_point_range = list(view_point_range)
         view_point_range[1] += 1
 
-        elements_of_viewpoint = (view_point_range[1] - view_point_range[0])
+        # elements_of_viewpoint = (view_point_range[1] - view_point_range[0])
+        # data_list = np.zeros(elements_of_viewpoint)
+        _view_point_start = view_point_start
+
+        # while _view_point_start <= view_point_end:
+        #     chrom, start, end, _ = self.hicMatrix.getBinPos(_view_point_start)
+        #     for j, idx in zip(range(elements_of_viewpoint), range(view_point_range[0], view_point_range[1], 1)):
+        #         data_list[j] += self.hicMatrix.matrix[_view_point_start, idx]
+
+        #     _view_point_start += 1
+
+
+
+
+          ### fix
+        elements_of_viewpoint  = max(self.hicMatrix.matrix.shape[0], self.hicMatrix.matrix.shape[1])
         data_list = np.zeros(elements_of_viewpoint)
+
         _view_point_start = view_point_start
 
         while _view_point_start <= view_point_end:
             chrom, start, end, _ = self.hicMatrix.getBinPos(_view_point_start)
-            for j, idx in zip(range(elements_of_viewpoint), range(view_point_range[0], view_point_range[1], 1)):
-                data_list[j] += self.hicMatrix.matrix[_view_point_start, idx]
+            for i in range(elements_of_viewpoint):
+                data_list[i] += self.hicMatrix.matrix[_view_point_start, i]
 
             _view_point_start += 1
+
+        # data_list = data_list[view_point_range[0]:view_point_range[1]]
+
 
         elements_of_viewpoint = elements_of_viewpoint - (view_point_end - view_point_start)
         data_list_new = np.zeros(elements_of_viewpoint)
 
-        index_before_viewpoint = view_point_start - view_point_range[0]
-
+        # index_before_viewpoint = view_point_start - view_point_range[0]
+        index_before_viewpoint = view_point_start
         # elements before the viewpoint
         data_list_new[0:index_before_viewpoint] = data_list[0:index_before_viewpoint]
 