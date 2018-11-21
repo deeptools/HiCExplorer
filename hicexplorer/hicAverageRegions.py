7aab5a0ce04078ba1aad5c93095b74a460e53c78
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2018-11-21 12:49:06 +0100

    average region plotting and support for bins added

--- hicexplorer/hicAverageRegions.py
+++ hicexplorer/hicAverageRegions.py
@@ -52,7 +52,7 @@ def parse_arguments(args=None):
 
 def calculateViewpointRange(pHiCMatrix, pViewpoint, pRange):
     '''
-    This function computes the correct start and end position of a viewpoint given the viewpoint and the range.
+    This function computes the correct start and end position of a viewpoint given the reference and the range.
     '''
 
 
@@ -74,38 +74,77 @@ def calculateViewpointRange(pHiCMatrix, pViewpoint, pRange):
 def getBinIndices(pHiCMatrix, pViewpoint):
     return pHiCMatrix.getRegionBinRange(pViewpoint[0], pViewpoint[1], pViewpoint[2])
 
+def calculateViewpointRangeBins(pHiCMatrix, pViewpoint, pRange):
+    
+    viewpoint_index = getBinIndices(pHiCMatrix, pViewpoint)[0]
+    start = viewpoint_index - pRange[0]
+    end = viewpoint_index + pRange[1]
+
+    return start, end
+    # log.debug('viewpoint_index {}'.format(viewpoint_index))
+    # log.debug('max_length {}'.format(max_length))
+# def extendMatrix(pStart, pEnd, pSize, pData):
+
+#     matrix = np.array((pSize, pSize))
+#     matrix[abs(pStart):pEnd, abs(pStart):pEnd] = pData
+#     return matrix
+
 def main():
 
     args = parse_arguments().parse_args()
 
     hic_ma = hm.hiCMatrix(pMatrixFile=args.matrix)
     indices_values = []
-    
+    # max_bin = hic_ma.matrix.shape[1]
     with open(args.regions, 'r') as file:
         for line in file.readlines():
             _line = line.strip().split('\t')
-            log.debug('_line {}'.format(_line))
+            # log.debug('_line {}'.format(_line))
             if len(line) == 0:
                 continue
             if len(_line) == 2:
                 chrom, start = _line[0], _line[1]
 
             viewpoint = (chrom, start, start)
-            log.debug('viewpoint {}'.format(viewpoint))
-            start_range_genomic, end_range_genomic, _ = calculateViewpointRange(hic_ma, viewpoint, args.range)
-            start_bin, end_bin = getBinIndices(hic_ma, (chrom, start_range_genomic, end_range_genomic))
+            if args.range:
+                start_range_genomic, end_range_genomic, _ = calculateViewpointRange(hic_ma, viewpoint, args.range)
+                start_bin, end_bin = getBinIndices(hic_ma, (chrom, start_range_genomic, end_range_genomic))
+            else:
+                start_bin, end_bin = calculateViewpointRangeBins(hic_ma, viewpoint, args.rangeInBins)
             indices_values.append([start_bin, end_bin])
+            # elif args.rangeInBins:
 
-    dimensions_new_matrix = (args.range[0] // hic_ma.getBinSize()) + (args.range[1] // hic_ma.getBinSize())
-
+    if args.range:
+        dimensions_new_matrix = (args.range[0] // hic_ma.getBinSize()) + (args.range[1] // hic_ma.getBinSize())
+    elif args.rangeInBins:
+        dimensions_new_matrix = args.rangeInBins[0] + args.rangeInBins[1]
     summed_matrix = csr_matrix((dimensions_new_matrix, dimensions_new_matrix), dtype=np.float32)
-    log.debug('indices_values {}'.format(indices_values))
-    log.debug('shaoe matrux {}'.format(summed_matrix.shape))
+    # log.debug('indices_values {}'.format(indices_values))
+    # log.debug('shaoe matrux {}'.format(summed_matrix.shape))
+    max_length = hic_ma.matrix.shape[1]
     for start, end in indices_values:
-        log.debug('shape {}'.format(hic_ma.matrix[start:end, start:end].shape))
-        log.debug('size; {}'.format(np.absolute(start-end)))
-        summed_matrix += hic_ma.matrix[start:end, start:end]
+        # log.debug('shape {}'.format(hic_ma.matrix[start:end, start:end].shape))
+        # log.debug('size; {}'.format(np.absolute(start-end)))
+        _start = 0
+        _end = summed_matrix.shape[1]
+        if start < 0:
+            log.debug('start')
+            _start = np.absolute(start)
+            start = 0
+            # matrix = hic_ma.matrix[start:end, start:end]
+        if end >= max_length:
+            log.debug('end')
+
+            _end = end
+            end = max_length
+            # matrix = hic_ma.matrix[start:end, start:end]
+
+        log.debug('summed_matrix[_start:_end, _start:_end].shape {}'.format(summed_matrix[_start:_end, _start:_end].shape))
+        log.debug('hic_ma.matrix[start:end, start:end].shape {}'.format(hic_ma.matrix[start:end, start:end].shape))
+        
+        summed_matrix[_start:_end, _start:_end] += hic_ma.matrix[start:end, start:end]
 
     summed_matrix /= len(indices_values)
 
+    
     save_npz(args.outFileName, summed_matrix)
