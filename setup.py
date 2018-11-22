dcca49a0381195253be1ba35bccb411baeaf7a17
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2018-11-22 14:46:51 +0000

    Updating dependency list

--- setup.py
+++ setup.py
@@ -93,21 +93,24 @@ class install(_install):
             sys.stderr.write("Error: {}".format(e))
 
 
-install_requires_py = ["numpy == 1.13.*",
-                       "scipy == 1.0.*",
-                       "matplotlib == 2.1.*",
-                       "pysam >= 0.11.2",
+install_requires_py = ["numpy >= 1.15.*",
+                       "scipy >= 1.1.*",
+                       "matplotlib >= 3.0.*",
+                       "pysam >= 0.14",
                        "intervaltree == 2.1.*",
-                       "biopython >= 1.68",
-                       "tables == 3.3.*",
-                       "pandas == 0.20.*",
-                       "pyBigWig == 0.3.*",
-                       "six == 1.10.*",
-                       "future == 0.16.*",
-                       "cooler == 0.7.*",
-                       "jinja2 == 2.9.*",
-                       "unidecode == 0.4.*",
-                       "hicmatrix == 2.2"
+                       "biopython >= 1.72",
+                       "tables >= 3.4.*",
+                       "pandas >= 0.23.*",
+                       "pyBigWig >= 0.3.*",
+                       "six >= 1.11.*",
+                       "future >= 0.17.*",
+                       "cooler >= 0.7.11*",
+                       "jinja2 >= 2.10.*",
+                       "unidecode >= 1.0.*",
+                       "hicmatrix >= 5",
+                       "pygenometracks >= 2.1",
+                       "scikit-learn >= 0.20",
+                       "psutil >= 5.4.8"
                        ]
 
 if sys.version_info[0] == 2:
