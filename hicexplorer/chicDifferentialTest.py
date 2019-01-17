69e80d6c3c4068891a6879a51a0383bee3363a7c
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2019-01-17 13:49:38 +0000

    More header information

--- hicexplorer/chicDifferentialTest.py
+++ hicexplorer/chicDifferentialTest.py
@@ -31,13 +31,13 @@ def parse_arguments(args=None):
                                 nargs=2)
 
     parserRequired.add_argument('--alpha', '-a',
-                           help='Accept all samples to significance level alpha',
-                           type=float,
-                           default=0.05,
-                           required=True)
+                                help='Accept all samples to significance level alpha',
+                                type=float,
+                                default=0.05,
+                                required=True)
     parserRequired.add_argument('--outFileName', '-o',
-                            help='File name to save the test results',
-                            required=True)
+                                help='File name to save the test results',
+                                required=True)
     parserOpt = parser.add_argument_group('Optional arguments')
     parserOpt.add_argument('--useData',
                            help='Type of data used for testing: raw interactions, relative interactions, rbz-score',
@@ -69,22 +69,22 @@ def readInteractionFile(pInteractionFile, pUseData):
         header = file.readline()
 
         for line in file.readlines():
-            _line = line.strip().split('\t')			
+            _line = line.strip().split('\t')
             line_content.append(_line)
             data.append([float(_line[data_selector_viewpoint]), float(_line[data_selector_target])])
-            
+
     return header_significance_level, header, line_content, data
-    
+
 
 def chisquare_test(pDataFile1, pDataFile2, pAlpha):
     # pair of accepted/unaccepted and pvalue
     # True is rejection of H0
     # False acceptance of H0
-    test_result = []    
+    test_result = []
     # Find the critical value for alpha confidence level
-    critical_value = stats.chi2.ppf(q = 1 - pAlpha, df = 1) 
+    critical_value = stats.chi2.ppf(q=1 - pAlpha, df=1)
     for group1, group2 in zip(pDataFile1, pDataFile2):
-        chi2, p_value, dof, ex = chi2_contingency([group1,group2], correction=False)
+        chi2, p_value, dof, ex = chi2_contingency([group1, group2], correction=False)
         if chi2 >= critical_value:
             test_result.append((True, p_value))
         else:
@@ -92,34 +92,56 @@ def chisquare_test(pDataFile1, pDataFile2, pAlpha):
 
     return test_result
 
-def writeResult(pOutFileName, pData, pRejected, pHeaderOld, pHeaderNew, pViewpoint1, pViewpoint2):
-    
+
+def writeResult(pOutFileName, pData, pRejected, pHeaderOld, pHeaderNew, pViewpoint1, pViewpoint2, pAlpha, pDof, pUsedData, pRbzSignificanceLevel):
+
     with open(pOutFileName, 'w') as file:
-        header = '# Differential analysis result file of HiCExplorer\'s chicDifferentialTest\n'
-        header += '# This file contains the regions accepted as differential by chi-squared contingency test (H0 was rejected) \n'
-        header += ' '.join(['# Used viewpoints regions: ' , str(pViewpoint1) , ' and ' , str(pViewpoint2) , '\n'])
-        header += '# Line 1 of a group contains data of viewpoint and target of sample 1, line 2 contains data of viewpoint and target of sample 2 \n'
-        header += '# line 3 the p-value of the chi-squared  contingency test.\n'
+        header = '# Differential analysis result file of HiCExplorer\'s chicDifferentialTest version '
+        header += str(__version__)
+        header += '\n'
+
+        if pRejected:
+            header += '# This file contains the regions accepted as differential by chi-squared contingency test (H0 was rejected) \n'
+        else:
+            header += '# This file contains the regions rejected as differential by chi-squared contingency test (H0 was accepted) \n'
+
+        header += ' '.join(['# Used viewpoints regions: ', ' '.join(pViewpoint1), ' and ', ' '.join(pViewpoint2), '\n'])
         header += '#\n'
+
+        header += '# Line 1 of a group contains data of viewpoint and target of sample 1, line 2 contains data of viewpoint and target of sample 2 \n'
+        header += '# line 3 the p-value of the chi-squared contingency test.\n'
+        header += '#\n'
+        header += ' '.join(['# Alpha level', str(pAlpha)])
+        header += '\n'
+        header += ' '.join(['# Degrees of freedom', str(pDof)])
+        header += '\n'
+
+        header += ''.join(['# Used data: ', str(pUsedData)])
+        header += '\n'
+
+        header += str(pRbzSignificanceLevel)
+        header += '\n\n'
+
         file.write(header)
         file.write(pHeaderOld)
 
         for data in pData:
-            file.write(str(data[0]) + '\n' + str(data[1]) + '\n' + format(data[2], '10.5f'))
+            file.write('\t'.join(data[0]) + '\n' + '\t'.join(data[1]) + '\n' + format(data[2], '10.5f') + '\n')
             file.write('\n')
 
 
 def main(args=None):
     args = parse_arguments().parse_args(args)
     # log.debug('muh')
-    
+
     header_significance_level1, header1, line_content1, data1 = readInteractionFile(args.interactionFile[0], args.useData)
+    # log.debug('{}'.format(header_significance_level1))
     header_significance_level2, header2, line_content2, data2 = readInteractionFile(args.interactionFile[1], args.useData)
 
     test_result = chisquare_test(data1, data2, args.alpha)
 
     rejected_h0 = []
-    
+
     non_rejected_h0 = []
     for i, result in enumerate(test_result):
         if result[0]:
@@ -127,17 +149,14 @@ def main(args=None):
         else:
             non_rejected_h0.append([line_content1[i], line_content2[i], result[1]])
 
-
     header_new = args.interactionFile[0]
     header_new += ' '
     header_new += args.interactionFile[1]
 
-    rejected_h0_str = 'Rejected H0 data'
+    outFileName = args.outFileName.split('.')
 
-    non_rejected_h0_str = 'Accepted H0 data'
+    outRejectedH0 = outFileName[0] + '_rejected_H0.bed'
+    outAcceptedH0 = outFileName[0] + '_accepted_H0.bed'
 
-    args.interactionFile[0]
-
-    args.interactionFile[0]
-    writeResult('outname_h0.bed', rejected_h0, rejected_h0_str, header1, header2, line_content1[0][:4], line_content2[0][:4])
-    writeResult('outname_h1.bed', non_rejected_h0, non_rejected_h0_str, header1, header2, line_content1[0][:4], line_content2[0][:4])
+    writeResult(outRejectedH0, rejected_h0, True, header1, header2, line_content1[0][:4], line_content2[0][:4], args.alpha, '1', args.useData, header_significance_level1)
+    writeResult(outAcceptedH0, non_rejected_h0, False, header1, header2, line_content1[0][:4], line_content2[0][:4], args.alpha, '1', args.useData, header_significance_level1)
