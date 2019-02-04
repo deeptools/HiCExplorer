3821f4b622b3ff53032c1c71032760abc9a1a414
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2019-02-04 23:20:51 +0100

    Disable log of cooler and matplotlib

--- hicexplorer/__init__.py
+++ hicexplorer/__init__.py
@@ -1,5 +1,8 @@
 import logging
 logging.basicConfig(level=logging.DEBUG)
+logging.getLogger('matplotlib').setLevel(logging.WARNING)
+logging.getLogger('cooler').setLevel(logging.WARNING)
+
 import warnings
 import sys
 
