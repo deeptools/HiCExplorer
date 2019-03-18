b75b9704da7970ae909695ff2af5cfbd0d39d7f7
Leily Rabbani <rabbani@pc390.ie-freiburg.mpg.de>
2019-03-18 12:15:29 +0100

    more on utilities

--- hicexplorer/utilities.py
+++ hicexplorer/utilities.py
@@ -478,4 +478,4 @@ def remove_non_ascii(pText):
 def check_cooler(pFileName):
     if pFileName.endswith('.cool') or cooler.fileops.is_cooler(pFileName) or'.mcool' in pFileName:
         return True
-return False
+    return False
