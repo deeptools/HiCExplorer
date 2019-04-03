2f1a72c2f718cc21c70e447396e7923cce7ca520
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2019-04-03 10:52:51 +0200

    Fixing mocking

--- docs/conf.py
+++ docs/conf.py
@@ -34,7 +34,6 @@ MOCK_MODULES = ['numpy', 'numpy.core', 'numpy.core.multiarray', 'numpy.distutils
 from unittest.mock import Mock
 
 
-
 # class Mock_New(Mock):
 #     @classmethod
 #     def __init__(self):
@@ -124,7 +123,6 @@ language = None
 exclude_patterns = ['_build']
 
 
-
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
 
@@ -142,4 +140,3 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
     import sphinx_rtd_theme
     html_theme = 'sphinx_rtd_theme'
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-
