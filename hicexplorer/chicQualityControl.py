052478cd3daa9a74292912f7299bd080b40d9a6c
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2019-05-22 16:22:13 +0000

    adding cHi-C qc

---
+++ hicexplorer/chicQualityControl.py
@@ -0,0 +1,221 @@
+import argparse
+import math
+from multiprocessing import Process, Queue
+import time
+import logging
+log = logging.getLogger(__name__)
+
+import numpy as np
+import fit_nbinom
+from scipy.stats import nbinom, kstest
+from statsmodels.stats.gof import gof_chisquare_discrete
+
+from hicmatrix import HiCMatrix as hm
+from hicexplorer._version import __version__
+from .lib import Viewpoint
+
+
+def parse_arguments(args=None):
+
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        add_help=False,
+        description="""
+                    Build a background model for viewpoints, the viewpoints over all samples (matrices) are used to build the background model.
+                    An example usage is:
+                    $ chicViewpointBackgroundModel -m matrix1.h5 matrix2.h5 matrix3.h5 -rp referencePointsFile.bed --range 20000 40000
+                    """
+    )
+
+    parserRequired = parser.add_argument_group('Required arguments')
+
+    parserRequired.add_argument('--matrices', '-m',
+                                help='The input matrices to build the background model on.',
+                                nargs='+',
+                                required=True)
+
+    parserRequired.add_argument('--referencePoints', '-rp',
+                                help='Bed file contains all reference points which should be used to build the background model.',
+                                type=str,
+                                required=True)
+
+    parserOpt = parser.add_argument_group('Optional arguments')
+    parserOpt.add_argument('--averageContactBin',
+                           help='Average the contacts of n bins, written to last column.',
+                           type=int,
+                           default=5)
+    parserOpt.add_argument('--outFileName', '-o',
+                           help='The name of the outputfile',
+                           default='new_referencepoints.bed')
+    parserOpt.add_argument('--threads', '-t',
+                           help='Number of threads. Using the python multiprocessing module. ',
+                           required=False,
+                           default=4,
+                           type=int
+                           )
+    parserOpt.add_argument('--fixateRange', '-fs',
+                           help='Fixate score of backgroundmodel starting at distance x. E.g. all values greater 500kb are set to the value of the 500kb bin.',
+                           required=False,
+                           default=500000,
+                           type=int
+                           )
+    parserOpt.add_argument('--help', '-h', action='help', help='show this help message and exit')
+
+    parserOpt.add_argument('--version', action='version',
+                           version='%(prog)s {}'.format(__version__))
+
+    return parser
+
+
+def compute_background(pReferencePoints, pViewpointObj, pArgs, pQueue):
+
+    # background_model_data = {}
+    # relative_positions = set()
+    test_result = []
+    average_count = []
+    undecided = []
+    for i, referencePoint in enumerate(pReferencePoints):
+
+        region_start, region_end, _ = pViewpointObj.calculateViewpointRange(referencePoint, (pArgs.fixateRange, pArgs.fixateRange))
+
+        data_list = pViewpointObj.computeViewpoint(referencePoint, referencePoint[0], region_start, region_end)
+        
+        # set data in relation to viewpoint, upstream are negative values, downstream positive, zero is viewpoint
+        view_point_start, _ = pViewpointObj.getReferencePointAsMatrixIndices(referencePoint)
+        view_point_range_start, view_point_range_end = \
+            pViewpointObj.getViewpointRangeAsMatrixIndices(referencePoint[0], region_start, region_end)
+
+        for i, data in zip(range(view_point_range_start, view_point_range_end, 1), data_list):
+            relative_position = i - view_point_start
+        zero_share = 1 - (np.count_nonzero(data_list) / len(data_list))
+
+        if pArgs.averageContactBin > 0:
+            data_list = pViewpointObj.smoothInteractionValues(data_list, pArgs.averageContactBin)
+
+        # fit_nb_parameters = fit_nbinom.fit(np.array(data_list))
+        # fit_nb_parameters_array = [fit_nb_parameters['size'], fit_nb_parameters['prob']]
+        # # np.savetxt('foo.data', np.array(data_list))
+        # # log.debug('fit_nb_parameters {}'.format(fit_nb_parameters))
+        # # nb_distribution = nbinom(fit_nb_parameters['size'], fit_nb_parameters['prob'])
+        # test_result.append(gof_chisquare_discrete(nbinom, arg=fit_nb_parameters_array, rvs=data_list, alpha=0.1, msg=referencePoint))
+        # # log.debug('test_result {}'.format(test_result))
+        # exit()
+        test_result.append(zero_share)
+        average_count.append(np.average(data_list))
+    with open('zero_share.txt', 'w') as file:
+        for i, referencePoint in enumerate(pReferencePoints):
+            file.write('{} zero share: {} {} {} {} {} \n'.format(referencePoint, test_result[i], test_result[i] > 0.95 , average_count[i], average_count[i] < 0.1, test_result[i] > 0.95 and average_count[i] < 0.1))
+            if test_result[i] > 0.95 != average_count[i] < 0.1:
+                undecided.append('{} {} {}\n'.format(referencePoint, test_result[i], average_count[i]))
+    with open('undecided.txt', 'w') as file:
+        for undecided_ in undecided:
+            file.write(undecided_)
+    pQueue.put([None, None])
+    return
+
+
+def main():
+    args = parse_arguments().parse_args()
+
+    viewpointObj = Viewpoint()
+    referencePoints, _ = viewpointObj.readReferencePointFile(args.referencePoints)
+
+    relative_counts_conditions = []
+
+    relative_positions = set()
+    bin_size = 0
+
+    # - compute for each condition (matrix):
+    # - all viewpoints and smooth them: sliding window approach
+    # - after smoothing, sum all viewpoints up to one
+    # - compute the percentage of each position with respect to the total interaction count
+    # for models of all conditions:
+    # - compute nbinom parameters
+
+    referencePointsPerThread = len(referencePoints) // args.threads
+    queue = [None] * args.threads
+    process = [None] * args.threads
+    background_model_data = None
+
+    for matrix in args.matrices:
+        hic_ma = hm.hiCMatrix(matrix)
+        viewpointObj.hicMatrix = hic_ma
+
+        bin_size = hic_ma.getBinSize()
+        all_data_collected = False
+        thread_done = [False] * args.threads
+        log.debug('matrix read, starting processing')
+        for i in range(args.threads):
+
+            if i < args.threads - 1:
+                referencePointsThread = referencePoints[i * referencePointsPerThread:(i + 1) * referencePointsPerThread]
+            else:
+                referencePointsThread = referencePoints[i * referencePointsPerThread:]
+
+            queue[i] = Queue()
+            process[i] = Process(target=compute_background, kwargs=dict(
+                pReferencePoints=referencePointsThread,
+                pViewpointObj=viewpointObj,
+                pArgs=args,
+                pQueue=queue[i]
+            )
+            )
+
+            process[i].start()
+
+        while not all_data_collected:
+            for i in range(args.threads):
+                if queue[i] is not None and not queue[i].empty():
+                    background_data_thread = queue[i].get()
+                    background_model_data_thread, relative_positions_thread = background_data_thread
+                    if background_model_data is None:
+                        background_model_data = background_model_data_thread
+                    else:
+                        for relativePosition in background_model_data_thread:
+                            if relativePosition in background_model_data:
+                                background_model_data[relativePosition].extend(background_model_data_thread[relativePosition])
+                            else:
+                                background_model_data[relativePosition] = background_model_data_thread[relativePosition]
+
+                    relative_positions = relative_positions.union(relative_positions_thread)
+                    queue[i] = None
+                    process[i].join()
+                    process[i].terminate()
+                    process[i] = None
+                    thread_done[i] = True
+            all_data_collected = True
+            for thread in thread_done:
+                if not thread:
+                    all_data_collected = False
+            time.sleep(1)
+
+        del hic_ma
+        del viewpointObj.hicMatrix
+
+    # for models of all conditions:
+    # - fit negative binomial for each relative distance
+    # mean_percentage = {}
+    # sem = {}
+    # relative_positions = sorted(relative_positions)
+    # nbinom_parameters = {}
+    # max_value = {}
+    # mean_value = {}
+    # sum_all_values = 0
+    # for relative_position in relative_positions:
+    #     nbinom_parameters[relative_position] = fit_nbinom.fit(np.array(background_model_data[relative_position]))
+    #     max_value[relative_position] = np.max(background_model_data[relative_position])
+    #     average_value = np.average(background_model_data[relative_position])
+    #     mean_value[relative_position] = average_value
+    #     sum_all_values += average_value
+
+    # for relative_position in relative_positions:
+    #     mean_value[relative_position] /= sum_all_values
+    # write result to file
+    # with open(args.outFileName, 'w') as file:
+    #     file.write('Relative position\tsize nbinom\tprob nbinom\tmax value\tmean value\n')
+        
+    #     for relative_position in relative_positions:
+    #         # if lower_limit_range <= relative_position and relative_position <= upper_limit_range:
+    #         relative_position_in_genomic_scale = relative_position * bin_size
+    #         file.write("{}\t{:.12f}\t{:.12f}\t{:.12f}\t{:.12f}\n".format(relative_position_in_genomic_scale, nbinom_parameters[relative_position]['size'],
+    #                                                    nbinom_parameters[relative_position]['prob'], max_value[relative_position], mean_value[relative_position]))
