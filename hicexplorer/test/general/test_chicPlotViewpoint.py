36259e433c89adad5107775c1f623bbba68df340
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2019-08-14 18:39:48 +0200

    Skeleton for test cases

---
+++ hicexplorer/test/general/test_chicPlotViewpoint.py
@@ -0,0 +1,38 @@
+import warnings
+warnings.simplefilter(action="ignore", category=RuntimeWarning)
+warnings.simplefilter(action="ignore", category=PendingDeprecationWarning)
+import pytest
+import os
+from tempfile import NamedTemporaryFile, mkdtemp
+import matplotlib as mpl
+mpl.use('agg')
+from matplotlib.testing.compare import compare_images
+
+from hicexplorer import chicPlotViewpoint
+ROOT = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "test_data/cHi-C/")
+
+def are_files_equal(file1, file2, delta=1, skip=0):
+    equal = True
+    if delta:
+        mismatches = 0
+    with open(file1) as textfile1, open(file2) as textfile2:
+        for i, (x, y) in enumerate(zip(textfile1, textfile2)):
+            # if x.startswith('File'):
+            #     continue
+            if i < skip:
+                continue
+            if x != y:
+                if delta:
+                    mismatches += 1
+                    if mismatches > delta:
+                        equal = False
+                        break
+                else:
+                    equal = False
+                    break
+    return equal
+
+def test_two_matrices():
+
+    res = compare_images(test_image_path, outfile.name, tolerance)
+    assert res is None, res
