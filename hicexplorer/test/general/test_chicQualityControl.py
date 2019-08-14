41b6ce4a0fa3e12a4aa8517a81b5d74a4fc945fe
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2019-08-14 18:38:30 +0200

    chi-c QC test case

---
+++ hicexplorer/test/general/test_chicQualityControl.py
@@ -0,0 +1,58 @@
+import warnings
+warnings.simplefilter(action="ignore", category=RuntimeWarning)
+warnings.simplefilter(action="ignore", category=PendingDeprecationWarning)
+import pytest
+import os
+from tempfile import NamedTemporaryFile, mkdtemp
+import matplotlib as mpl
+mpl.use('agg')
+from matplotlib.testing.compare import compare_images
+
+from hicexplorer import chicQualityControl
+ROOT = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "test_data/cHi-C/")
+
+def are_files_equal(file1, file2, delta=1, skip=0):
+    equal = True
+    if delta:
+        mismatches = 0
+    with open(file1) as textfile1, open(file2) as textfile2:
+        for i, (x, y) in enumerate(zip(textfile1, textfile2)):
+            # if x.startswith('File'):
+            #     continue
+            if i < skip:
+                continue
+            if x != y:
+                if delta:
+                    mismatches += 1
+                    if mismatches > delta:
+                        equal = False
+                        break
+                else:
+                    equal = False
+                    break
+    return equal
+
+def test_two_matrices():
+
+    outfile = NamedTemporaryFile(suffix='.bed', delete=False)
+    outfile_histogram = NamedTemporaryFile(suffix='.png', delete=False)
+    outfile_sparsity = NamedTemporaryFile(suffix='.png', delete=False)
+
+    outfile.close()
+    args = "--matrices {} {} --referencePoints {} --sparsity {} --outFileName {} --outFileNameHistogram {} --outFileNameSparsity  {}".format(ROOT + 'FL-E13-5_chr1.cool', 
+                                                                        ROOT + 'MB-E10-5_chr1.cool', 
+                                                                        ROOT + 'referencePoints.bed',
+                                                                        0.05,
+                                                                        outfile.name, outfile_histogram.name, outfile_sparsity.name).split()
+    chicQualityControl.main(args)
+
+    assert are_files_equal(ROOT + "chicQualityControl/new_referencepoints.bed", outfile.name)
+    assert are_files_equal(ROOT + "chicQualityControl/new_referencepoints.bed_raw_filter", outfile.name + '_raw_filter')
+    assert are_files_equal(ROOT + "chicQualityControl/new_referencepoints.bed_rejected_filter", outfile.name  + '_rejected_filter')
+
+    
+    res = compare_images(ROOT + "chicQualityControl/histogram.png", outfile_histogram.name, 50)
+    assert res is None, res
+
+    res = compare_images(ROOT + "chicQualityControl/sparsity.png", outfile_sparsity.name, 50)
+    assert res is None, res
