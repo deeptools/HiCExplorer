8aebbc1476e726457f1beccd8c0458ff01b20edd
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2019-08-15 18:52:46 +0200

    Add test cases for chicPlotViewpoint

--- hicexplorer/test/general/test_chicPlotViewpoint.py
+++ hicexplorer/test/general/test_chicPlotViewpoint.py
@@ -11,6 +11,9 @@ from matplotlib.testing.compare import compare_images
 from hicexplorer import chicPlotViewpoint
 ROOT = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "test_data/cHi-C/")
 
+tolerance = 50
+
+
 def are_files_equal(file1, file2, delta=1, skip=0):
     equal = True
     if delta:
@@ -32,7 +35,104 @@ def are_files_equal(file1, file2, delta=1, skip=0):
                     break
     return equal
 
-def test_two_matrices():
+# one viewpoint
 
-    res = compare_images(test_image_path, outfile.name, tolerance)
+
+def test_one_viewpoint():
+    outfile = NamedTemporaryFile(suffix='.png', delete=False)
+    outfile.close()
+    args = "-if {} --range {} {} -o {}".format(ROOT + 'chicViewpoint/output_1/FL-E13-5_chr1_chr1_14300280_14300280_Eya1.bed', 200000, 200000, outfile.name).split()
+    chicPlotViewpoint.main(args)
+
+    res = compare_images(ROOT + 'chicPlotViewpoint/FL-E13-5_chr1_chr1_14300280_14300280_Eya1.png', outfile.name, tolerance)
     assert res is None, res
+
+# n - viewpoints, jpg
+
+
+def test_two_viewpoint_fileformat_dpi():
+    outfile = NamedTemporaryFile(suffix='.pdf', delete=False)
+    outfile.close()
+    args = "-if {} {} --range {} {} -o {} --dpi {} --outputFormat {}".format(
+        ROOT + 'chicViewpoint/output_1/FL-E13-5_chr1_chr1_14300280_14300280_Eya1.bed',
+        ROOT + 'chicViewpoint/output_1/MB-E10-5_chr1_chr1_14300280_14300280_Eya1.bed',
+        200000, 200000, outfile.name, 100, 'pdf').split()
+    chicPlotViewpoint.main(args)
+
+    res = compare_images(ROOT + 'chicPlotViewpoint/two_viewpoint.pdf', outfile.name, tolerance)
+    assert res is None, res
+# additional background model
+
+
+def test_two_viewpoint_background():
+    outfile = NamedTemporaryFile(suffix='.png', delete=False)
+    outfile.close()
+    args = "-if {} {} --range {} {} -o {} -bmf {}".format(
+        ROOT + 'chicViewpoint/output_1/FL-E13-5_chr1_chr1_14300280_14300280_Eya1.bed',
+        ROOT + 'chicViewpoint/output_1/MB-E10-5_chr1_chr1_14300280_14300280_Eya1.bed',
+        200000, 200000, outfile.name, ROOT + 'background.bed').split()
+    chicPlotViewpoint.main(args)
+
+    res = compare_images(ROOT + 'chicPlotViewpoint/two_viewpoint_background.png', outfile.name, tolerance)
+    assert res is None, res
+
+
+def test_two_viewpoint_background_significant():
+    outfile = NamedTemporaryFile(suffix='.png', delete=False)
+    outfile.close()
+    args = "-if {} {} --range {} {} -o {} -bmf {} --significantInteractions {} {}".format(
+        ROOT + 'chicViewpoint/output_1/FL-E13-5_chr1_chr1_14300280_14300280_Eya1.bed',
+        ROOT + 'chicViewpoint/output_1/MB-E10-5_chr1_chr1_14300280_14300280_Eya1.bed',
+        200000, 200000, outfile.name, ROOT + 'background.bed',
+        ROOT + 'chicSignificantInteractions/output_3/FL-E13-5_chr1_chr1_14300280_14300280_Eya1_output_significant.bed',
+        ROOT + 'chicSignificantInteractions/output_3/MB-E10-5_chr1_chr1_14300280_14300280_Eya1_output_significant.bed').split()
+    chicPlotViewpoint.main(args)
+
+    res = compare_images(ROOT + 'chicPlotViewpoint/two_viewpoint_background_significant.png', outfile.name, tolerance)
+    assert res is None, res
+
+
+def test_two_viewpoint_background_differential():
+    outfile = NamedTemporaryFile(suffix='.png', delete=False)
+    outfile.close()
+    args = "-if {} {} --range {} {} -o {} -bmf {} --differentialTestResult {}".format(
+        ROOT + 'chicViewpoint/output_1/FL-E13-5_chr1_chr1_14300280_14300280_Eya1.bed',
+        ROOT + 'chicViewpoint/output_1/MB-E10-5_chr1_chr1_14300280_14300280_Eya1.bed',
+        200000, 200000, outfile.name, ROOT + 'background.bed',
+        ROOT + 'chicDifferentialTest/batch_mode_fisher/FL-E13-5_MB-E10-5_chr1_chr1_14300280_14300280_Eya1_H0_rejected.bed').split()
+    chicPlotViewpoint.main(args)
+
+    res = compare_images(ROOT + 'chicPlotViewpoint/two_viewpoint_background_differential.png', outfile.name, tolerance)
+    assert res is None, res
+
+
+def test_one_viewpoint_colormap_pvalue():
+    outfile = NamedTemporaryFile(suffix='.png', delete=False)
+    outfile.close()
+    args = "-if {} --range {} {} --pValue --colorMapPvalue {} -o {} --pValue --colorMapPvalue plasma --maxPValue 0.5 --minPValue 0.1".format(ROOT + 'chicViewpoint/output_1/FL-E13-5_chr1_chr1_14300280_14300280_Eya1.bed', 200000, 200000, 'plasma', outfile.name).split()
+    chicPlotViewpoint.main(args)
+
+    res = compare_images(ROOT + 'chicPlotViewpoint/two_viewpoint_pvalue.png', outfile.name, tolerance)
+    assert res is None, res
+
+# all above with batch mode
+
+# x-fold background
+
+
+def test_one_viewpoint_per_file_batch_mode():
+    output_folder = mkdtemp(prefix="output_")
+
+    args = "-if {} -iff {} --range {} {} --outputFolder {} -psn {} -bm".format(ROOT + 'chicViewpoint/fileNames_one_matrix.txt',
+                                                                               ROOT + 'chicViewpoint/output_4',
+                                                                               200000, 200000,
+                                                                               output_folder, 1).split()
+    chicPlotViewpoint.main(args)
+
+    res = compare_images(ROOT + 'chicPlotViewpoint/batchMode/one/Eya1_FL-E13-5_chr1_chr1_14300280.png', output_folder + '/Eya1_FL-E13-5_chr1_chr1_14300280.png', tolerance)
+    assert res is None, res
+    res = compare_images(ROOT + 'chicPlotViewpoint/batchMode/one/Sox17_FL-E13-5_chr1_chr1_4487435.png', output_folder + '/Sox17_FL-E13-5_chr1_chr1_4487435.png', tolerance)
+    assert res is None, res
+    res = compare_images(ROOT + 'chicPlotViewpoint/batchMode/one/Tfap2d_FL-E13-5_chr1_chr1_19093103.png', output_folder + '/Tfap2d_FL-E13-5_chr1_chr1_19093103.png', tolerance)
+    assert res is None, res
+    assert set(os.listdir(ROOT + "chicPlotViewpoint/batchMode/one")) == set(os.listdir(output_folder))
