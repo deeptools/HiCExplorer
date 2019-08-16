3d7adc8d67c5830984a91191fa4418411d58f43f
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2019-08-16 14:45:27 +0200

    Add one test case

--- hicexplorer/test/general/test_chicDifferentialTest.py
+++ hicexplorer/test/general/test_chicDifferentialTest.py
@@ -115,3 +115,32 @@ def test_batch_mode_chi2():
     assert are_files_equal(ROOT + "chicDifferentialTest/batch_mode_chi2/FL-E13-5_MB-E10-5_chr1_chr1_4487435_4487435_Sox17_results.bed", output_folder + '/FL-E13-5_MB-E10-5_chr1_chr1_4487435_4487435_Sox17_results.bed')
 
     assert set(os.listdir(ROOT + "chicDifferentialTest/batch_mode_chi2/")) == set(os.listdir(output_folder))
+
+
+def test_batch_mode_fisher():
+
+    output_folder = mkdtemp(prefix="output_")
+
+    args = "--interactionFile {} -iff {} --alpha {} --statisticTest {} --outputFolder {} -bm --rejectedFileNamesToFile {}\
+        ".format(ROOT + 'chicAggregateStatistic/batch_mode_file_names.txt',
+                 ROOT + 'chicAggregateStatistic/batch_mode',
+                 0.5, 'fisher',
+                 output_folder,
+                 ).split()
+    chicDifferentialTest.main(args)
+
+    assert are_files_equal(ROOT + "chicDifferentialTest/batch_mode_fisher_outfile/FL-E13-5_MB-E10-5_chr1_chr1_14300280_14300280_Eya1_H0_accepted.bed", output_folder + '/FL-E13-5_MB-E10-5_chr1_chr1_14300280_14300280_Eya1_H0_accepted.bed')
+    assert are_files_equal(ROOT + "chicDifferentialTest/batch_mode_fisher_outfile/FL-E13-5_MB-E10-5_chr1_chr1_14300280_14300280_Eya1_H0_rejected.bed", output_folder + '/FL-E13-5_MB-E10-5_chr1_chr1_14300280_14300280_Eya1_H0_rejected.bed')
+    assert are_files_equal(ROOT + "chicDifferentialTest/batch_mode_fisher_outfile/FL-E13-5_MB-E10-5_chr1_chr1_14300280_14300280_Eya1_results.bed", output_folder + '/FL-E13-5_MB-E10-5_chr1_chr1_14300280_14300280_Eya1_results.bed')
+
+    assert are_files_equal(ROOT + "chicDifferentialTest/batch_mode_fisher_outfile/FL-E13-5_MB-E10-5_chr1_chr1_19093103_19093103_Tfap2d_H0_accepted.bed", output_folder + '/FL-E13-5_MB-E10-5_chr1_chr1_19093103_19093103_Tfap2d_H0_accepted.bed')
+    assert are_files_equal(ROOT + "chicDifferentialTest/batch_mode_fisher_outfile/FL-E13-5_MB-E10-5_chr1_chr1_19093103_19093103_Tfap2d_H0_rejected.bed", output_folder + '/FL-E13-5_MB-E10-5_chr1_chr1_19093103_19093103_Tfap2d_H0_rejected.bed')
+    assert are_files_equal(ROOT + "chicDifferentialTest/batch_mode_fisher_outfile/FL-E13-5_MB-E10-5_chr1_chr1_19093103_19093103_Tfap2d_results.bed", output_folder + '/FL-E13-5_MB-E10-5_chr1_chr1_19093103_19093103_Tfap2d_results.bed')
+
+    assert are_files_equal(ROOT + "chicDifferentialTest/batch_mode_fisher_outfile/FL-E13-5_MB-E10-5_chr1_chr1_4487435_4487435_Sox17_H0_accepted.bed", output_folder + '/FL-E13-5_MB-E10-5_chr1_chr1_4487435_4487435_Sox17_H0_accepted.bed')
+    assert are_files_equal(ROOT + "chicDifferentialTest/batch_mode_fisher_outfile/FL-E13-5_MB-E10-5_chr1_chr1_4487435_4487435_Sox17_H0_rejected.bed", output_folder + '/FL-E13-5_MB-E10-5_chr1_chr1_4487435_4487435_Sox17_H0_rejected.bed')
+    assert are_files_equal(ROOT + "chicDifferentialTest/batch_mode_fisher_outfile/FL-E13-5_MB-E10-5_chr1_chr1_4487435_4487435_Sox17_results.bed", output_folder + '/FL-E13-5_MB-E10-5_chr1_chr1_4487435_4487435_Sox17_results.bed')
+
+    assert are_files_equal(ROOT + "chicDifferentialTest/batch_mode_fisher_outfile/rejectedFilesList.txt", output_folder + '/rejectedFilesList.txt')
+
+    assert set(os.listdir(ROOT + "chicDifferentialTest/batch_mode_fisher_outfile/")) == set(os.listdir(output_folder))
