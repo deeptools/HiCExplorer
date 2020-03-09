30391a547f3f9d6327111a1034048c7bdadc5bf1
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2020-03-09 11:24:52 +0100

    Set threads to 1 for chic test cases

--- hicexplorer/test/general/test_chicViewpoint.py
+++ hicexplorer/test/general/test_chicViewpoint.py
@@ -1,12 +1,13 @@
+from hicexplorer import chicViewpoint
+from tempfile import NamedTemporaryFile, mkdtemp
+import os
+import pytest
 import warnings
 warnings.simplefilter(action="ignore", category=RuntimeWarning)
 warnings.simplefilter(action="ignore", category=PendingDeprecationWarning)
-import pytest
-import os
-from tempfile import NamedTemporaryFile, mkdtemp
 
-from hicexplorer import chicViewpoint
-ROOT = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "test_data/cHi-C/")
+ROOT = os.path.join(os.path.dirname(os.path.dirname(
+    os.path.abspath(__file__))), "test_data/cHi-C/")
 
 
 def are_files_equal(file1, file2, delta=1, skip=0):
@@ -35,40 +36,51 @@ def test_two_matrices():
     outfile = NamedTemporaryFile(suffix='.txt', delete=False)
     output_folder = mkdtemp(prefix="output_")
     outfile.close()
-    args = "--matrices {} {} --referencePoints {} --backgroundModel {} --range {} {} -o {}".format(ROOT + 'FL-E13-5_chr1.cool',
-                                                                                                   ROOT + 'MB-E10-5_chr1.cool',
-                                                                                                   ROOT + 'referencePoints_chicViewpoint.bed',
-                                                                                                   ROOT + 'background.txt',
-                                                                                                   200000, 200000,
-                                                                                                   output_folder).split()
+    args = "--matrices {} {} --referencePoints {} --backgroundModel {} --range {} {} -o {} -t {}".format(ROOT + 'FL-E13-5_chr1.cool',
+                                                                                                         ROOT + 'MB-E10-5_chr1.cool',
+                                                                                                         ROOT + 'referencePoints_chicViewpoint.bed',
+                                                                                                         ROOT + 'background.txt',
+                                                                                                         200000, 200000,
+                                                                                                         output_folder, 1).split()
     chicViewpoint.main(args)
 
-    assert are_files_equal(ROOT + "chicViewpoint/output_1/FL-E13-5_chr1_chr1_4487435_4487435_Sox17.txt", output_folder + '/FL-E13-5_chr1_chr1_4487435_4487435_Sox17.txt', skip=4)
-    assert are_files_equal(ROOT + "chicViewpoint/output_1/FL-E13-5_chr1_chr1_14300280_14300280_Eya1.txt", output_folder + '/FL-E13-5_chr1_chr1_14300280_14300280_Eya1.txt', skip=4)
-    assert are_files_equal(ROOT + "chicViewpoint/output_1/FL-E13-5_chr1_chr1_19093103_19093103_Tfap2d.txt", output_folder + '/FL-E13-5_chr1_chr1_19093103_19093103_Tfap2d.txt', skip=4)
-    assert are_files_equal(ROOT + "chicViewpoint/output_1/MB-E10-5_chr1_chr1_4487435_4487435_Sox17.txt", output_folder + '/MB-E10-5_chr1_chr1_4487435_4487435_Sox17.txt', skip=4)
-    assert are_files_equal(ROOT + "chicViewpoint/output_1/MB-E10-5_chr1_chr1_14300280_14300280_Eya1.txt", output_folder + '/MB-E10-5_chr1_chr1_14300280_14300280_Eya1.txt', skip=4)
-    assert are_files_equal(ROOT + "chicViewpoint/output_1/MB-E10-5_chr1_chr1_19093103_19093103_Tfap2d.txt", output_folder + '/MB-E10-5_chr1_chr1_19093103_19093103_Tfap2d.txt', skip=4)
+    assert are_files_equal(ROOT + "chicViewpoint/output_1/FL-E13-5_chr1_chr1_4487435_4487435_Sox17.txt",
+                           output_folder + '/FL-E13-5_chr1_chr1_4487435_4487435_Sox17.txt', skip=4)
+    assert are_files_equal(ROOT + "chicViewpoint/output_1/FL-E13-5_chr1_chr1_14300280_14300280_Eya1.txt",
+                           output_folder + '/FL-E13-5_chr1_chr1_14300280_14300280_Eya1.txt', skip=4)
+    assert are_files_equal(ROOT + "chicViewpoint/output_1/FL-E13-5_chr1_chr1_19093103_19093103_Tfap2d.txt",
+                           output_folder + '/FL-E13-5_chr1_chr1_19093103_19093103_Tfap2d.txt', skip=4)
+    assert are_files_equal(ROOT + "chicViewpoint/output_1/MB-E10-5_chr1_chr1_4487435_4487435_Sox17.txt",
+                           output_folder + '/MB-E10-5_chr1_chr1_4487435_4487435_Sox17.txt', skip=4)
+    assert are_files_equal(ROOT + "chicViewpoint/output_1/MB-E10-5_chr1_chr1_14300280_14300280_Eya1.txt",
+                           output_folder + '/MB-E10-5_chr1_chr1_14300280_14300280_Eya1.txt', skip=4)
+    assert are_files_equal(ROOT + "chicViewpoint/output_1/MB-E10-5_chr1_chr1_19093103_19093103_Tfap2d.txt",
+                           output_folder + '/MB-E10-5_chr1_chr1_19093103_19093103_Tfap2d.txt', skip=4)
 
-    assert set(os.listdir(ROOT + "chicViewpoint/output_1/")) == set(os.listdir(output_folder))
+    assert set(os.listdir(ROOT + "chicViewpoint/output_1/")
+               ) == set(os.listdir(output_folder))
 
 
 def test_one_matrix():
     outfile = NamedTemporaryFile(suffix='.txt', delete=False)
     output_folder = mkdtemp(prefix="output_")
     outfile.close()
-    args = "--matrices {} --referencePoints {} --backgroundModel {} --range {} {} -o {}".format(ROOT + 'FL-E13-5_chr1.cool',
-                                                                                                ROOT + 'referencePoints_chicViewpoint.bed',
-                                                                                                ROOT + 'background.txt',
-                                                                                                200000, 200000,
-                                                                                                output_folder).split()
+    args = "--matrices {} --referencePoints {} --backgroundModel {} --range {} {} -o {} -t {}".format(ROOT + 'FL-E13-5_chr1.cool',
+                                                                                                      ROOT + 'referencePoints_chicViewpoint.bed',
+                                                                                                      ROOT + 'background.txt',
+                                                                                                      200000, 200000,
+                                                                                                      output_folder, 1).split()
     chicViewpoint.main(args)
 
-    assert are_files_equal(ROOT + "chicViewpoint/output_2/FL-E13-5_chr1_chr1_4487435_4487435_Sox17.txt", output_folder + '/FL-E13-5_chr1_chr1_4487435_4487435_Sox17.txt', skip=4)
-    assert are_files_equal(ROOT + "chicViewpoint/output_2/FL-E13-5_chr1_chr1_14300280_14300280_Eya1.txt", output_folder + '/FL-E13-5_chr1_chr1_14300280_14300280_Eya1.txt', skip=4)
-    assert are_files_equal(ROOT + "chicViewpoint/output_2/FL-E13-5_chr1_chr1_19093103_19093103_Tfap2d.txt", output_folder + '/FL-E13-5_chr1_chr1_19093103_19093103_Tfap2d.txt', skip=4)
+    assert are_files_equal(ROOT + "chicViewpoint/output_2/FL-E13-5_chr1_chr1_4487435_4487435_Sox17.txt",
+                           output_folder + '/FL-E13-5_chr1_chr1_4487435_4487435_Sox17.txt', skip=4)
+    assert are_files_equal(ROOT + "chicViewpoint/output_2/FL-E13-5_chr1_chr1_14300280_14300280_Eya1.txt",
+                           output_folder + '/FL-E13-5_chr1_chr1_14300280_14300280_Eya1.txt', skip=4)
+    assert are_files_equal(ROOT + "chicViewpoint/output_2/FL-E13-5_chr1_chr1_19093103_19093103_Tfap2d.txt",
+                           output_folder + '/FL-E13-5_chr1_chr1_19093103_19093103_Tfap2d.txt', skip=4)
 
-    assert set(os.listdir(ROOT + "chicViewpoint/output_2/")) == set(os.listdir(output_folder))
+    assert set(os.listdir(ROOT + "chicViewpoint/output_2/")
+               ) == set(os.listdir(output_folder))
 
 
 def test_two_matrices_writeFileName():
@@ -77,23 +89,31 @@ def test_two_matrices_writeFileName():
 
     output_folder = mkdtemp(prefix="output_")
     outfile.close()
-    args = "--matrices {} {} --referencePoints {} --backgroundModel {} --range {} {} -o {} -w {}".format(ROOT + 'FL-E13-5_chr1.cool',
-                                                                                                         ROOT + 'MB-E10-5_chr1.cool',
-                                                                                                         ROOT + 'referencePoints_chicViewpoint.bed',
-                                                                                                         ROOT + 'background.txt',
-                                                                                                         200000, 200000,
-                                                                                                         output_folder, outfile_name_list.name).split()
+    args = "--matrices {} {} --referencePoints {} --backgroundModel {} --range {} {} -o {} -w {} -t {}".format(ROOT + 'FL-E13-5_chr1.cool',
+                                                                                                               ROOT + 'MB-E10-5_chr1.cool',
+                                                                                                               ROOT + 'referencePoints_chicViewpoint.bed',
+                                                                                                               ROOT + 'background.txt',
+                                                                                                               200000, 200000,
+                                                                                                               output_folder, outfile_name_list.name, 1).split()
     chicViewpoint.main(args)
-    assert are_files_equal(ROOT + "chicViewpoint/fileNames_two_matrices.txt", outfile_name_list.name)
+    assert are_files_equal(
+        ROOT + "chicViewpoint/fileNames_two_matrices.txt", outfile_name_list.name)
 
-    assert are_files_equal(ROOT + "chicViewpoint/output_3/FL-E13-5_chr1_chr1_4487435_4487435_Sox17.txt", output_folder + '/FL-E13-5_chr1_chr1_4487435_4487435_Sox17.txt', skip=4)
-    assert are_files_equal(ROOT + "chicViewpoint/output_3/FL-E13-5_chr1_chr1_14300280_14300280_Eya1.txt", output_folder + '/FL-E13-5_chr1_chr1_14300280_14300280_Eya1.txt', skip=4)
-    assert are_files_equal(ROOT + "chicViewpoint/output_3/FL-E13-5_chr1_chr1_19093103_19093103_Tfap2d.txt", output_folder + '/FL-E13-5_chr1_chr1_19093103_19093103_Tfap2d.txt', skip=4)
-    assert are_files_equal(ROOT + "chicViewpoint/output_3/MB-E10-5_chr1_chr1_4487435_4487435_Sox17.txt", output_folder + '/MB-E10-5_chr1_chr1_4487435_4487435_Sox17.txt', skip=4)
-    assert are_files_equal(ROOT + "chicViewpoint/output_3/MB-E10-5_chr1_chr1_14300280_14300280_Eya1.txt", output_folder + '/MB-E10-5_chr1_chr1_14300280_14300280_Eya1.txt', skip=4)
-    assert are_files_equal(ROOT + "chicViewpoint/output_3/MB-E10-5_chr1_chr1_19093103_19093103_Tfap2d.txt", output_folder + '/MB-E10-5_chr1_chr1_19093103_19093103_Tfap2d.txt', skip=4)
+    assert are_files_equal(ROOT + "chicViewpoint/output_3/FL-E13-5_chr1_chr1_4487435_4487435_Sox17.txt",
+                           output_folder + '/FL-E13-5_chr1_chr1_4487435_4487435_Sox17.txt', skip=4)
+    assert are_files_equal(ROOT + "chicViewpoint/output_3/FL-E13-5_chr1_chr1_14300280_14300280_Eya1.txt",
+                           output_folder + '/FL-E13-5_chr1_chr1_14300280_14300280_Eya1.txt', skip=4)
+    assert are_files_equal(ROOT + "chicViewpoint/output_3/FL-E13-5_chr1_chr1_19093103_19093103_Tfap2d.txt",
+                           output_folder + '/FL-E13-5_chr1_chr1_19093103_19093103_Tfap2d.txt', skip=4)
+    assert are_files_equal(ROOT + "chicViewpoint/output_3/MB-E10-5_chr1_chr1_4487435_4487435_Sox17.txt",
+                           output_folder + '/MB-E10-5_chr1_chr1_4487435_4487435_Sox17.txt', skip=4)
+    assert are_files_equal(ROOT + "chicViewpoint/output_3/MB-E10-5_chr1_chr1_14300280_14300280_Eya1.txt",
+                           output_folder + '/MB-E10-5_chr1_chr1_14300280_14300280_Eya1.txt', skip=4)
+    assert are_files_equal(ROOT + "chicViewpoint/output_3/MB-E10-5_chr1_chr1_19093103_19093103_Tfap2d.txt",
+                           output_folder + '/MB-E10-5_chr1_chr1_19093103_19093103_Tfap2d.txt', skip=4)
 
-    assert set(os.listdir(ROOT + "chicViewpoint/output_3/")) == set(os.listdir(output_folder))
+    assert set(os.listdir(ROOT + "chicViewpoint/output_3/")
+               ) == set(os.listdir(output_folder))
 
 
 def test_one_matrix_writeFileName():
@@ -102,16 +122,21 @@ def test_one_matrix_writeFileName():
 
     output_folder = mkdtemp(prefix="output_")
     outfile.close()
-    args = "--matrices {} --referencePoints {} --backgroundModel {} --range {} {} -o {} -w {}".format(ROOT + 'FL-E13-5_chr1.cool',
-                                                                                                      ROOT + 'referencePoints_chicViewpoint.bed',
-                                                                                                      ROOT + 'background.txt',
-                                                                                                      200000, 200000,
-                                                                                                      output_folder, outfile_name_list.name).split()
+    args = "--matrices {} --referencePoints {} --backgroundModel {} --range {} {} -o {} -w {} -t {}".format(ROOT + 'FL-E13-5_chr1.cool',
+                                                                                                            ROOT + 'referencePoints_chicViewpoint.bed',
+                                                                                                            ROOT + 'background.txt',
+                                                                                                            200000, 200000,
+                                                                                                            output_folder, outfile_name_list.name, 1).split()
     chicViewpoint.main(args)
-    assert are_files_equal(ROOT + "chicViewpoint/fileNames_one_matrix.txt", outfile_name_list.name)
+    assert are_files_equal(
+        ROOT + "chicViewpoint/fileNames_one_matrix.txt", outfile_name_list.name)
 
-    assert are_files_equal(ROOT + "chicViewpoint/output_4/FL-E13-5_chr1_chr1_4487435_4487435_Sox17.txt", output_folder + '/FL-E13-5_chr1_chr1_4487435_4487435_Sox17.txt', skip=4)
-    assert are_files_equal(ROOT + "chicViewpoint/output_4/FL-E13-5_chr1_chr1_14300280_14300280_Eya1.txt", output_folder + '/FL-E13-5_chr1_chr1_14300280_14300280_Eya1.txt', skip=4)
-    assert are_files_equal(ROOT + "chicViewpoint/output_4/FL-E13-5_chr1_chr1_19093103_19093103_Tfap2d.txt", output_folder + '/FL-E13-5_chr1_chr1_19093103_19093103_Tfap2d.txt', skip=4)
+    assert are_files_equal(ROOT + "chicViewpoint/output_4/FL-E13-5_chr1_chr1_4487435_4487435_Sox17.txt",
+                           output_folder + '/FL-E13-5_chr1_chr1_4487435_4487435_Sox17.txt', skip=4)
+    assert are_files_equal(ROOT + "chicViewpoint/output_4/FL-E13-5_chr1_chr1_14300280_14300280_Eya1.txt",
+                           output_folder + '/FL-E13-5_chr1_chr1_14300280_14300280_Eya1.txt', skip=4)
+    assert are_files_equal(ROOT + "chicViewpoint/output_4/FL-E13-5_chr1_chr1_19093103_19093103_Tfap2d.txt",
+                           output_folder + '/FL-E13-5_chr1_chr1_19093103_19093103_Tfap2d.txt', skip=4)
 
-    assert set(os.listdir(ROOT + "chicViewpoint/output_4/")) == set(os.listdir(output_folder))
+    assert set(os.listdir(ROOT + "chicViewpoint/output_4/")
+               ) == set(os.listdir(output_folder))
