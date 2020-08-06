b1c374cf07e185110f001267a8560c95bea51f7a
Leily Rabbani <rabbani@pc390.ie-freiburg.mpg.de>
2020-08-06 17:18:44 +0200

    removed a duplicated line

--- hicexplorer/hicAdjustMatrix.py
+++ hicexplorer/hicAdjustMatrix.py
@@ -139,7 +139,6 @@ def adjustMatrix(pArgs):
             hic_matrix.orig_bin_ids = []
             hic_matrix.orig_cut_intervals = []
             hic_matrix.nan_bins = []
-            hic_matrix.orig_cut_intervals = []
 
     elif pArgs.maskBadRegions:
         if check_cooler(pArgs.matrix) and len(pArgs.chromosomes) == 1 and pArgs.action == 'keep':
