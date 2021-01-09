4daf094d35d09adc9b7af40a0bf6ede889efec31
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2021-01-09 16:16:19 +0100

    start to implement hdf support

--- hicexplorer/chicDifferentialTest.py
+++ hicexplorer/chicDifferentialTest.py
@@ -10,6 +10,7 @@ log = logging.getLogger(__name__)
 
 import numpy as np
 from scipy import stats
+import h5py
 
 import hicmatrix.HiCMatrix as hm
 from hicexplorer import utilities
@@ -45,10 +46,9 @@ This will create, as in the non-batch mode, three files per aggregated file and 
 
     parserRequired = parser.add_argument_group('Required arguments')
 
-    parserRequired.add_argument('--interactionFile', '-if',
-                                help='path to the interaction files which should be used for the differential test.',
-                                required=True,
-                                nargs='+')
+    parserRequired.add_argument('--aggregatedFile', '-if',
+                                help='path to the aggregated files which should be used for the differential test.',
+                                required=True)
 
     parserRequired.add_argument('--alpha', '-a',
                                 help='define a significance level (alpha) for accepting samples',
@@ -57,25 +57,25 @@ This will create, as in the non-batch mode, three files per aggregated file and 
 
     parserOpt = parser.add_argument_group('Optional arguments')
 
-    parserOpt.add_argument('--interactionFileFolder', '-iff',
-                           help='Folder where the interaction files are stored. Applies only for batch mode'
-                           ' (Default: %(default)s).',
-                           required=False,
-                           default='.')
-    parserOpt.add_argument('--outputFolder', '-o',
-                           help='Output folder of the files'
-                           ' (Default: %(default)s).',
-                           required=False,
-                           default='differentialResults')
+    # parserOpt.add_argument('--interactionFileFolder', '-iff',
+    #                        help='Folder where the interaction files are stored. Applies only for batch mode'
+    #                        ' (Default: %(default)s).',
+    #                        required=False,
+    #                        default='.')
+    # parserOpt.add_argument('--outputFolder', '-o',
+    #                        help='Output folder of the files'
+    #                        ' (Default: %(default)s).',
+    #                        required=False,
+    #                        default='differentialResults')
     parserOpt.add_argument('--statisticTest',
                            help='Type of test used: fisher\'s exact test or chi2 contingency'
                            ' (Default: %(default)s).',
                            choices=['fisher', 'chi2'],
                            default='fisher')
-    parserOpt.add_argument('--batchMode', '-bm',
-                           help='turn on batch mode. The given file for --interactionFile and or --targetFile contain a list of the to be processed files.',
-                           required=False,
-                           action='store_true')
+    # parserOpt.add_argument('--batchMode', '-bm',
+    #                        help='turn on batch mode. The given file for --interactionFile and or --targetFile contain a list of the to be processed files.',
+    #                        required=False,
+    #                        action='store_true')
     parserOpt.add_argument('--threads', '-t',
                            help='Number of threads (uses the python multiprocessing module)'
                            ' (Default: %(default)s).',
@@ -116,6 +116,28 @@ def readInteractionFile(pInteractionFile):
 
     return header, line_content, data
 
+def readAggregatedFileHDF(pAggregatedFileName, pInternalPath):
+    aggregatedFileHDF5Object = h5py.File(pAggregatedFileName, 'r')
+
+    internal_path = '/'.join(pInternalPath)
+    chromosome = aggregatedFileHDF5Object.get( internal_path + '/' + 'chromosome')[()].decode("utf-8")
+    
+    gene_name = aggregatedFileHDF5Object.get( internal_path + '/' + 'chromosome')[()].decode("utf-8")
+    actions = np.array(aggregatedFileHDF5Object[internal_path + '/' + array_name][:])
+   
+    start_list = np.array(aggregatedFileHDF5Object[internal_path + '/' + array_name][:])
+    end_list = np.array(aggregatedFileHDF5Object[internal_path + '/' + array_name][:])
+    relative_distance_list = np.array(aggregatedFileHDF5Object[internal_path + '/' + array_name][:])
+    raw_target_list = np.array(aggregatedFileHDF5Object[internal_path + '/' + array_name][:])
+    
+    # chromosome = None
+    #     start_list = []
+    #     end_list = []
+    #     gene_name = None
+    #     sum_of_interactions = None
+    #     relative_distance_list = []
+       
+    #     raw_target_list = [] 
 
 def chisquare_test(pDataFile1, pDataFile2, pAlpha):
     # pair of accepted/unaccepted and pvalue
@@ -215,36 +237,36 @@ def run_statistical_tests(pInteractionFilesList, pArgs, pQueue=None):
     try:
         for interactionFile in pInteractionFilesList:
 
-            sample_prefix = interactionFile[0].split(
-                '/')[-1].split('_')[0] + '_' + interactionFile[1].split('/')[-1].split('_')[0]
+            # sample_prefix = interactionFile[0].split(
+            #     '/')[-1].split('_')[0] + '_' + interactionFile[1].split('/')[-1].split('_')[0]
 
-            region_prefix = '_'.join(
-                interactionFile[0].split('/')[-1].split('_')[1:6])
+            # region_prefix = '_'.join(
+            #     interactionFile[0].split('/')[-1].split('_')[1:6])
 
-            outFileName = sample_prefix + '_' + region_prefix
-            rejected_name_output_file = outFileName + '_H0_rejected.txt'
+            # outFileName = sample_prefix + '_' + region_prefix
+            # rejected_name_output_file = outFileName + '_H0_rejected.txt'
 
-            if pArgs.outputFolder != '.':
-                outFileName_accepted = pArgs.outputFolder + \
-                    '/' + outFileName + '_H0_accepted.txt'
-                outFileName_rejected = pArgs.outputFolder + \
-                    '/' + outFileName + '_H0_rejected.txt'
-                outFileName = pArgs.outputFolder + '/' + outFileName + '_results.txt'
-            else:
-                outFileName_accepted = outFileName + '_H0_accepted.txt'
-                outFileName_rejected = outFileName + '_H0_rejected.txt'
-                outFileName = outFileName + '_results.txt'
+            # if pArgs.outputFolder != '.':
+            #     outFileName_accepted = pArgs.outputFolder + \
+            #         '/' + outFileName + '_H0_accepted.txt'
+            #     outFileName_rejected = pArgs.outputFolder + \
+            #         '/' + outFileName + '_H0_rejected.txt'
+            #     outFileName = pArgs.outputFolder + '/' + outFileName + '_results.txt'
+            # else:
+            #     outFileName_accepted = outFileName + '_H0_accepted.txt'
+            #     outFileName_rejected = outFileName + '_H0_rejected.txt'
+            #     outFileName = outFileName + '_results.txt'
 
-            if pArgs.interactionFileFolder != '.':
-                absolute_sample_path1 = pArgs.interactionFileFolder + '/' + interactionFile[0]
-                absolute_sample_path2 = pArgs.interactionFileFolder + '/' + interactionFile[1]
+            # if pArgs.interactionFileFolder != '.':
+            #     absolute_sample_path1 = pArgs.interactionFileFolder + '/' + interactionFile[0]
+            #     absolute_sample_path2 = pArgs.interactionFileFolder + '/' + interactionFile[1]
 
-            else:
-                absolute_sample_path1 = interactionFile[0]
-                absolute_sample_path2 = interactionFile[1]
+            # else:
+            #     absolute_sample_path1 = interactionFile[0]
+            #     absolute_sample_path2 = interactionFile[1]
 
-            header1, line_content1, data1 = readInteractionFile(absolute_sample_path1)
-            header2, line_content2, data2 = readInteractionFile(absolute_sample_path2)
+            header1, line_content1, data1 = readAggregatedFileHDF(pArgs.aggregatedFile, interactionFile[0])
+            header2, line_content2, data2 = readAggregatedFileHDF(pArgs.aggregatedFile, interactionFile[0])
 
             if len(line_content1) == 0 or len(line_content2) == 0:
                 writeResult(outFileName, None, header1, header2,
@@ -277,13 +299,13 @@ def run_statistical_tests(pInteractionFilesList, pArgs, pQueue=None):
                 write_out_lines_rejected.append(
                     [line_content1[result[0]], line_content2[result[0]], result[1], data1[result[0]], data2[result[0]]])
 
-            writeResult(outFileName, write_out_lines, header1, header2,
-                        pArgs.alpha, pArgs.statisticTest)
-            writeResult(outFileName_accepted, write_out_lines_accepted, header1, header2,
-                        pArgs.alpha, pArgs.statisticTest)
-            writeResult(outFileName_rejected, write_out_lines_rejected, header1, header2,
-                        pArgs.alpha, pArgs.statisticTest)
-            rejected_names.append(rejected_name_output_file)
+            # writeResult(outFileName, write_out_lines, header1, header2,
+            #             pArgs.alpha, pArgs.statisticTest)
+            # writeResult(outFileName_accepted, write_out_lines_accepted, header1, header2,
+            #             pArgs.alpha, pArgs.statisticTest)
+            # writeResult(outFileName_rejected, write_out_lines_rejected, header1, header2,
+            #             pArgs.alpha, pArgs.statisticTest)
+            # rejected_names.append(rejected_name_output_file)
 
     except Exception as exp:
         pQueue.put('Fail: ' + str(exp))
@@ -297,87 +319,123 @@ def run_statistical_tests(pInteractionFilesList, pArgs, pQueue=None):
 
 def main(args=None):
     args = parse_arguments().parse_args(args)
-    if not os.path.exists(args.outputFolder):
-        try:
-            os.makedirs(args.outputFolder)
-        except OSError as exc:  # Guard against race condition
-            if exc.errno != errno.EEXIST:
-                raise
-    interactionFileList = []
-    if args.batchMode:
-        with open(args.interactionFile[0], 'r') as interactionFile:
-            file_ = True
-            while file_:
-                # for line in fh.readlines():
-                file_ = interactionFile.readline().strip()
-                file2_ = interactionFile.readline().strip()
-                if file_ != '' and file2_ != '':
-                    interactionFileList.append((file_, file2_))
-            log.debug('len(interactionFileList) {}'.format(len(interactionFileList)))
-    else:
-        if len(args.interactionFile) % 2 == 0:
+    # if not os.path.exists(args.outputFolder):
+    #     try:
+    #         os.makedirs(args.outputFolder)
+    #     except OSError as exc:  # Guard against race condition
+    #         if exc.errno != errno.EEXIST:
+    #             raise
 
-            i = 0
-            while i < len(args.interactionFile):
-                interactionFileList.append(
-                    (args.interactionFile[i], args.interactionFile[i + 1]))
-                i += 2
+
+    aggregatedList = []
+
+
+    aggregatedFileHDF5Object = h5py.File(args.aggregatedFile, 'r')
+    keys_aggregatedFile = list(aggregatedFileHDF5Object.keys()) 
+
+    if len(keys_aggregatedFile) > 1:
+        for i, sample in enumerate(keys_aggregatedFile):
+            for sample2 in keys_aggregatedFile[i + 1:]:
+                
+                matrix_obj1 = aggregatedFileHDF5Object[sample]
+                matrix_obj2 = aggregatedFileHDF5Object[sample]
+
+                chromosomeList1 = sorted(list(matrix_obj1.keys()))
+                chromosomeList2 = sorted(list(matrix_obj2.keys()))
+                chromosomeList1.remove('genes')
+                chromosomeList2.remove('genes')
+                for chromosome1, chromosome2 in zip(chromosomeList1, chromosomeList2):
+                    geneList1 = sorted(list(matrix_obj1[chromosome1].keys()))
+                    geneList2 = sorted(list(matrix_obj2[chromosome2].keys()))
+
+                    for gene1, gene2 in zip(geneList1, geneList2):
+                        # if gene1 in present_genes[sample][sample2]:
+                        aggregatedList.append([[sample,chromosome1, gene1],[sample2,chromosome2, gene2]])
+
+            # for viewpoint, viewpoint2 in zip(sample, sample2):
+            #     writeFileNamesToList.append(viewpoint.encode("ascii", "ignore"))
+            #     writeFileNamesToList.append(viewpoint2.encode("ascii", "ignore"))
+    # log.debug(interactionList)
+    else:
+        log.error('To aggregate and prepare the data for the differential test, at least two matrices need to be stored, but only one is present.')
+    aggregatedFileHDF5Object.close()
+
+    log.debug('aggregatedList {}'.format(aggregatedList))
+    
+    # if args.batchMode:
+    #     with open(args.interactionFile[0], 'r') as interactionFile:
+    #         file_ = True
+    #         while file_:
+    #             # for line in fh.readlines():
+    #             file_ = interactionFile.readline().strip()
+    #             file2_ = interactionFile.readline().strip()
+    #             if file_ != '' and file2_ != '':
+    #                 interactionFileList.append((file_, file2_))
+    #         log.debug('len(interactionFileList) {}'.format(len(interactionFileList)))
+    # else:
+    #     if len(args.interactionFile) % 2 == 0:
+
+    #         i = 0
+    #         while i < len(args.interactionFile):
+    #             interactionFileList.append(
+    #                 (args.interactionFile[i], args.interactionFile[i + 1]))
+    #             i += 2
 
     fail_flag = False
     fail_message = ''
-    if args.batchMode:
-        rejected_file_names = [None] * args.threads
-        interactionFilesPerThread = len(interactionFileList) // args.threads
-        all_data_collected = False
-        queue = [None] * args.threads
-        process = [None] * args.threads
-        thread_done = [False] * args.threads
-        length_of_threads = 0
+    # if args.batchMode:
+    rejected_file_names = [None] * args.threads
+    aggregatedListPerThread = len(aggregatedList) // args.threads
+    all_data_collected = False
+    queue = [None] * args.threads
+    process = [None] * args.threads
+    thread_done = [False] * args.threads
+    length_of_threads = 0
+    for i in range(args.threads):
+
+        if i < args.threads - 1:
+            aggregatedListThread = aggregatedList[i * aggregatedListPerThread:(i + 1) * aggregatedListPerThread]
+        else:
+            aggregatedListThread = aggregatedList[i * aggregatedListPerThread:]
+        length_of_threads += len(aggregatedListThread)
+        queue[i] = Queue()
+        process[i] = Process(target=run_statistical_tests, kwargs=dict(
+            pInteractionFilesList=aggregatedListThread,
+            pArgs=args,
+            pQueue=queue[i]
+        )
+        )
+
+        process[i].start()
+    log.debug('length_of_threads {}'.format(length_of_threads))
+    while not all_data_collected:
         for i in range(args.threads):
+            if queue[i] is not None and not queue[i].empty():
+                background_data_thread = queue[i].get()
+                if 'Fail:' in background_data_thread:
+                    fail_flag = True
+                    fail_message = background_data_thread[6:]
+                rejected_file_names[i] = background_data_thread
+                queue[i] = None
+                process[i].join()
+                process[i].terminate()
+                process[i] = None
+                thread_done[i] = True
+        all_data_collected = True
+        for thread in thread_done:
+            if not thread:
+                all_data_collected = False
+        time.sleep(1)
+    if fail_flag:
+        log.error(fail_message)
+        exit(1)
+    # else:
+    #     run_statistical_tests(interactionFileList, args)
 
-            if i < args.threads - 1:
-                interactionFileListThread = interactionFileList[i * interactionFilesPerThread:(i + 1) * interactionFilesPerThread]
-            else:
-                interactionFileListThread = interactionFileList[i * interactionFilesPerThread:]
-            length_of_threads += len(interactionFileListThread)
-            queue[i] = Queue()
-            process[i] = Process(target=run_statistical_tests, kwargs=dict(
-                pInteractionFilesList=interactionFileListThread,
-                pArgs=args,
-                pQueue=queue[i]
-            )
-            )
+    # if args.batchMode:
+    #     log.debug('rejected_file_names {}'.format(len(rejected_file_names)))
+    #     rejected_file_names = [item for sublist in rejected_file_names for item in sublist]
+    #     log.debug('rejected_file_names II {}'.format(len(rejected_file_names)))
 
-            process[i].start()
-        log.debug('length_of_threads {}'.format(length_of_threads))
-        while not all_data_collected:
-            for i in range(args.threads):
-                if queue[i] is not None and not queue[i].empty():
-                    background_data_thread = queue[i].get()
-                    if 'Fail:' in background_data_thread:
-                        fail_flag = True
-                        fail_message = background_data_thread[6:]
-                    rejected_file_names[i] = background_data_thread
-                    queue[i] = None
-                    process[i].join()
-                    process[i].terminate()
-                    process[i] = None
-                    thread_done[i] = True
-            all_data_collected = True
-            for thread in thread_done:
-                if not thread:
-                    all_data_collected = False
-            time.sleep(1)
-        if fail_flag:
-            log.error(fail_message)
-            exit(1)
-    else:
-        run_statistical_tests(interactionFileList, args)
-
-    if args.batchMode:
-        log.debug('rejected_file_names {}'.format(len(rejected_file_names)))
-        rejected_file_names = [item for sublist in rejected_file_names for item in sublist]
-        log.debug('rejected_file_names II {}'.format(len(rejected_file_names)))
-
-        with open(args.rejectedFileNamesToFile, 'w') as nameListFile:
-            nameListFile.write('\n'.join(rejected_file_names))
+    #     with open(args.rejectedFileNamesToFile, 'w') as nameListFile:
+    #         nameListFile.write('\n'.join(rejected_file_names))
